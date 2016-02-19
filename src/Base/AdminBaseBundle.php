961e04921d28d42dbd3d5c9d4c3c667841badaa9
Glynn Forrest <me@glynnforrest.com>
2016-02-19 10:42:53 +0000

    First bundle

---
+++ src/Base/AdminBaseBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Admin\Base;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * AdminBaseBundle
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class AdminBaseBundle extends Bundle
+{
+}
