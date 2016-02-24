e128a1383d85dc86af2d1f5cc20bb2dd16456847
Glynn Forrest <me@glynnforrest.com>
2016-02-24 10:13:22 +0000

    Base template up and running

--- src/Base/Gruntfile.js
+++ src/Base/Gruntfile.js
@@ -13,10 +13,17 @@ module.exports = function(grunt) {
         files: 'Resources/scss/*.scss',
         tasks: ['sass']
       }
+    },
+    symlink: {
+      explicit: {
+        src: 'Resources/vendor/font-awesome/fonts',
+        dest: 'Resources/public/fa-fonts'
+      },
     }
   });
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-symlink');
   grunt.registerTask('default',['watch']);
-  grunt.registerTask('build',['sass']);
+  grunt.registerTask('build',['symlink', 'sass']);
 }
