a9a999337e5ad077fadd7b20ae2022137806058d
Glynn Forrest <me@glynnforrest.com>
2016-02-27 00:01:32 +0000

    Crud actions without @Template get _template
    Looks for the usual controller view, then falls back to
    AdminBaseBundle:Crud views.

---
+++ src/Base/EventListener/CrudTemplateListener.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Admin\Base\EventListener;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Admin\Base\Controller\CrudController;
+
+/**
+ * Allows crud controllers to optionally override crud templates.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ */
+class CrudTemplateListener
+{
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Chooses the correct template name to use if the controller is a crud controller action.
+     *
+     * @param FilterControllerEvent $event A FilterControllerEvent instance
+     */
+    public function onKernelController(FilterControllerEvent $event)
+    {
+        if (!is_array($controller = $event->getController())) {
+            return;
+        }
+
+        if (!$controller[0] instanceof CrudController) {
+            return;
+        }
+
+        $request = $event->getRequest();
+        //don't override @Template annotation
+        if ($request->attributes->has('_template')) {
+            return;
+        }
+
+        $guesser = $this->container->get('sensio_framework_extra.view.guesser');
+        $template = $guesser->guessTemplateName($controller, $request);
+        $templating = $this->container->get('templating');
+
+        if (!$templating->exists($template)) {
+            //remove Action
+            $segment = substr($controller[1], 0, -6);
+            $template = 'AdminBaseBundle:Crud:'.$segment.'.html.twig';
+        }
+
+        $request->attributes->set('_template', $template);
+    }
+}
