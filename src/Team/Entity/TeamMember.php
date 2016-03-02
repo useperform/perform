aaf46f6e3f5288f6b8adcb0e9b8e8931e252e3d2
Glynn Forrest <me@glynnforrest.com>
2016-03-02 17:41:36 +0000

    Add TeamMember entity

---
+++ src/Team/Entity/TeamMember.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Admin\Team\Entity;
+
+/**
+ * TeamMember
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class TeamMember
+{
+    /**
+     * @var uuid
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Role / job title in the team
+     *
+     * @var string
+     */
+    protected $role;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @var int
+     */
+    protected $sortOrder;
+
+    /**
+     * @return uuid
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return TeamMember
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $role
+     *
+     * @return TeamMember
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string $description
+     *
+     * @return TeamMember
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param int $sortOrder
+     *
+     * @return TeamMember
+     */
+    public function setSortOrder($sortOrder)
+    {
+        $this->sortOrder = (int) $sortOrder;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSortOrder()
+    {
+        return $this->sortOrder;
+    }
+}
