6743f95bc6ec41d39a5879f113f4703200328b1f
Glynn Forrest <me@glynnforrest.com>
2016-03-13 13:27:43 +0000

    Start AdminMediaBundle

---
+++ src/MediaBundle/AdminMediaBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Admin\MediaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * AdminMediaBundle
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class AdminMediaBundle extends Bundle
+{
+}
