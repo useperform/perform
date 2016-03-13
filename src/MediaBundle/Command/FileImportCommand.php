5275e525c4be2ac7830efa82c68d17758cfc5e68
Glynn Forrest <me@glynnforrest.com>
2016-03-13 14:18:02 +0000

    Add FileImportCommand

---
+++ src/MediaBundle/Command/FileImportCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Admin\MediaBundle\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+/**
+ * FileImportCommand
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ */
+class FileImportCommand extends ContainerAwareCommand
+{
+    protected $name = 'admin:media:import';
+    protected $description = 'Add files to the media library.';
+
+    protected function configure()
+    {
+        $this->setName($this->name)
+             ->setDescription($this->description)
+             ->addArgument(
+                 'path',
+                 InputArgument::REQUIRED,
+                 'The path to the file or directory'
+             );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $importer = $this->getContainer()->get('admin_media.importer.file');
+        foreach ($this->getFiles($input->getArgument('path')) as $file) {
+            $importer->importFile($file->getPathname());
+            $output->writeln(sprintf('Imported <info>%s</info>', $file->getPathname()));
+        }
+    }
+
+    protected function getFiles($path)
+    {
+        if (!is_dir($path)) {
+            return [new \SplFileObject($path)];
+        }
+
+        return new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(
+                $path, RecursiveDirectoryIterator::SKIP_DOTS
+            )
+        );
+    }
+}
