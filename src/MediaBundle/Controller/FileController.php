761f2655c51183b3efc77634f42dfc7134f94d55
Glynn Forrest <me@glynnforrest.com>
2016-03-13 13:28:35 +0000

    Start file controller

---
+++ src/MediaBundle/Controller/FileController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Admin\MediaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * FileController
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class FileController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template
+     */
+    public function listAction()
+    {
+        $files = $this->getDoctrine()->getRepository('AdminMediaBundle:File')->findAll();
+
+        return [
+            'files' => $files,
+        ];
+    }
+
+    /**
+     * @Route("/upload")
+     * @Template
+     */
+    public function uploadAction()
+    {
+        return [
+
+        ];
+    }
+}
