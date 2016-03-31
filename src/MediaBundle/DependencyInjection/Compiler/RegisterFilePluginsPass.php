c256ba79c5ed2f8ab4e6e463e353aaf188997d7c
Glynn Forrest <me@glynnforrest.com>
2016-03-31 16:55:49 +0100

    Making sure order of media plugins is preserved

--- src/MediaBundle/DependencyInjection/Compiler/RegisterFilePluginsPass.php
+++ src/MediaBundle/DependencyInjection/Compiler/RegisterFilePluginsPass.php
@@ -18,21 +18,27 @@ class RegisterFilePluginsPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         $registry = $container->getDefinition('admin_media.plugin.registry');
+        $pluginServices = [];
+
+        foreach ($container->findTaggedServiceIds('admin_media.file_plugin') as $service => $tag) {
+            if (!isset($tag[0]['pluginName'])) {
+                throw new \InvalidArgumentException(sprintf('The service %s tagged with "admin_media.file_plugin" must set the "pluginName" option in the tag.', $service));
+            }
+            $pluginServices[$tag[0]['pluginName']] = $service;
+        }
 
         // is there a better way to get the resolved configuration without having to do it manually?
         $processor = new Processor();
         $config = $processor->processConfiguration(new Configuration(), $container->getExtensionConfig('admin_media'));
         $plugins = $config['plugins'];
 
-        foreach ($container->findTaggedServiceIds('admin_media.file_plugin') as $service => $tag) {
-            if (!isset($tag[0]['pluginName'])) {
-                throw new \InvalidArgumentException(sprintf('The service %s tagged with "admin_media.file_plugin" must set the "pluginName" option in the tag.', $service));
-            }
-            if (!in_array($tag[0]['pluginName'], $plugins)) {
-                continue;
+
+        foreach ($plugins as $plugin) {
+            if (!isset($pluginServices[$plugin])) {
+                throw new PluginNotFoundException(sprintf('"%s" plugin not found while loading admin_media configuration.', $plugin));
             }
 
-            $registry->addMethodCall('addPlugin', [new Reference($service)]);
+            $registry->addMethodCall('addPlugin', [new Reference($pluginServices[$plugin])]);
         }
     }
 }
