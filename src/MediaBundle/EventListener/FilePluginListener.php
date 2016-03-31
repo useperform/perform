37b9513c28855532e467c1af99c9a9ee3e1129e2
Glynn Forrest <me@glynnforrest.com>
2016-03-31 16:03:49 +0100

    Add listener to use plugins when adding / removing files

---
+++ src/MediaBundle/EventListener/FilePluginListener.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Admin\MediaBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Admin\MediaBundle\Event\FileEvent;
+use Admin\MediaBundle\Plugin\PluginRegistry;
+
+/**
+ * FilePluginListener
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class FilePluginListener implements EventSubscriberInterface
+{
+    public function __construct(PluginRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    public function onFileCreate(FileEvent $event)
+    {
+        return $this->registry->onFileCreate($event->getFile());
+    }
+
+    public function onFileProcess(FileEvent $event)
+    {
+        return $this->registry->onFileProcess($event->getFile());
+    }
+
+    public function onFileDelete(FileEvent $event)
+    {
+        return $this->registry->onFileDelete($event->getFile());
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            FileEvent::CREATE => ['onFileCreate'],
+            FileEvent::PROCESS => ['onFileProcess'],
+            FileEvent::DELETE => ['onFileDelete'],
+        ];
+    }
+}
