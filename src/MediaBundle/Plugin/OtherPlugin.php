bc8eabd17b019c08d62366278238d011a4223645
Glynn Forrest <me@glynnforrest.com>
2016-03-31 16:08:50 +0100

    Add base media plugin 'other'

---
+++ src/MediaBundle/Plugin/OtherPlugin.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Admin\MediaBundle\Plugin;
+
+use Admin\MediaBundle\Entity\File;
+use Admin\MediaBundle\Url\FileUrlGeneratorInterface;
+use League\Flysystem\FilesystemInterface;
+
+/**
+ * OtherPlugin.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class OtherPlugin implements FilePluginInterface
+{
+    protected $type = 'other';
+    protected $storage;
+    protected $urlGenerator;
+
+    public function __construct(FilesystemInterface $storage, FileUrlGeneratorInterface $urlGenerator)
+    {
+        $this->storage = $storage;
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function getName()
+    {
+        return $this->type;
+    }
+
+    public function getListingName()
+    {
+        return 'Other';
+    }
+
+    public function getUrl(File $file)
+    {
+        return $this->urlGenerator->getUrl($file);
+    }
+
+    public function getPreview(File $file)
+    {
+        return '<i class="fa fa-file-o"></i>';
+    }
+
+    public function onCreate(File $file)
+    {
+        $file->setType($this->type);
+    }
+
+    public function onProcess(File $file)
+    {
+    }
+
+    public function onDelete(File $file)
+    {
+    }
+}
