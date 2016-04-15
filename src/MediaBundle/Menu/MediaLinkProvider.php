7c8956e54aec963d092f20cbc3292a4c37db25e7
Glynn Forrest <me@glynnforrest.com>
2016-04-15 03:52:58 +0100

    Add media link provider

---
+++ src/MediaBundle/Menu/MediaLinkProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Admin\MediaBundle\Menu;
+
+use Knp\Menu\ItemInterface;
+use Admin\Base\Menu\LinkProviderInterface;
+
+/**
+ * MediaLinkProvider.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class MediaLinkProvider implements LinkProviderInterface
+{
+    public function addLinks(ItemInterface $menu)
+    {
+        $media = $menu->addChild('Media', [
+            'uri' => '#'
+        ])->setExtra('icon', 'briefcase');
+        $media->addChild('List', [
+            'route' => 'admin_media_file_list'
+        ]);
+        $media->addChild('Import', [
+            'route' => 'admin_media_file_upload'
+        ]);
+    }
+}
