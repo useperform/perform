5f7de2b427838f0f456124d15b86845b206f8f56
Glynn Forrest <me@glynnforrest.com>
2016-04-15 03:52:21 +0100

    Add compiler pass to register menu link providers

--- src/Base/DependencyInjection/Compiler/ConfigureMenuPass.php
+++ src/Base/DependencyInjection/Compiler/ConfigureMenuPass.php
@@ -4,6 +4,7 @@ namespace Admin\Base\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * Setup the admin menu
@@ -13,5 +14,20 @@ class ConfigureMenuPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         $container->setParameter('knp_menu.renderer.twig.template', 'AdminBaseBundle::menu.html.twig');
+
+        $services = [];
+        foreach ($container->findTaggedServiceIds('admin_base.link_provider') as $service => $tag) {
+            if (!isset($tag[0]['alias'])) {
+                throw new \InvalidArgumentException(sprintf('The service %s tagged with "admin_base.link_provider" must set the "alias" option in the tag.', $service));
+            }
+            $alias = $tag[0]['alias'];
+            $services[] = $service;
+        }
+        //sort services by admin_base.menu.order in the config file, if defined
+
+        $definition = $container->getDefinition('admin_base.menu_builder');
+        foreach ($services as $service) {
+            $definition->addMethodCall('addLinkProvider', [new Reference($service)]);
+        }
     }
 }
