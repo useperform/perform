2cc656ad2b9dc04ac21e57830dce06a0aaff7db5
Glynn Forrest <me@glynnforrest.com>
2016-04-19 18:03:18 +0100

    Add TypeRegistry as a service

--- src/Base/DependencyInjection/AdminBaseExtension.php
+++ src/Base/DependencyInjection/AdminBaseExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * AdminBaseExtension.
@@ -19,5 +20,9 @@ class AdminBaseExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $definition = $container->register('admin_base.type_registry', 'Admin\Base\Type\TypeRegistry');
+        $definition->addArgument(new Reference('service_container'));
+        $definition->addMethodCall('addType', ['string', 'Admin\Base\Type\StringType']);
     }
 }
