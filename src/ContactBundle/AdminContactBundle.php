0fd8b0f373e5a20889640a0207ad52ace6dd1877
Glynn Forrest <me@glynnforrest.com>
2016-04-19 13:52:34 +0100

    Start admin contact bundle

---
+++ src/ContactBundle/AdminContactBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Admin\ContactBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * AdminContactBundle
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class AdminContactBundle extends Bundle
+{
+}
