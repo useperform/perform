30c7acebc691df9c522461561258a8be6e824352
Glynn Forrest <me@glynnforrest.com>
2016-04-19 17:18:46 +0100

    Add TypeRegistry

---
+++ src/Base/Exception/TypeNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Admin\Base\Exception;
+
+/**
+ * TypeNotFoundException
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class TypeNotFoundException extends \Exception
+{
+}
