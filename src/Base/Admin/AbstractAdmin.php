d1d607c8e4df66311da614fa63c25284eee6443f
Glynn Forrest <me@glynnforrest.com>
2016-04-19 17:48:22 +0100

    AbstractAdmin now returns lists of fields with options

--- src/Base/Admin/AbstractAdmin.php
+++ src/Base/Admin/AbstractAdmin.php
@@ -16,26 +16,50 @@ abstract class AbstractAdmin implements AdminInterface
     protected $viewFields = [];
     protected $createFields = [];
     protected $editFields = [];
+    protected $fieldOptions = [];
+    protected $listFieldOptions = [];
+    protected $viewFieldOptions = [];
+    protected $createFieldOptions = [];
+    protected $editFieldOptions = [];
     protected $routePrefix;
 
     public function getListFields()
     {
-        return $this->listFields;
+        return $this->resolveFields($this->listFields, $this->listFieldOptions);
     }
 
     public function getViewFields()
     {
-        return $this->viewFields;
+        return $this->resolveFields($this->viewFields, $this->viewFieldOptions);
     }
 
     public function getCreateFields()
     {
-        return $this->createFields;
+        return $this->resolveFields($this->createFields, $this->createFieldOptions);
     }
 
     public function getEditFields()
     {
-        return $this->editFields;
+        return $this->resolveFields($this->editFields, $this->editFieldOptions);
+    }
+
+    protected function resolveFields(array $fields, array $overrideFields)
+    {
+        $results = [];
+        foreach ($fields as $field) {
+            $options = isset($this->fieldOptions[$field]) ? $this->fieldOptions[$field] : [];
+            if (isset($overrideFields[$field])) {
+                $options = array_merge($options, $overrideFields[$field]);
+            }
+
+            if (!isset($options['type'])) {
+                $options['type'] = 'string';
+            }
+
+            $results[$field] = $options;
+        }
+
+        return $results;
     }
 
     public function getFormType()
