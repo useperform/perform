8aa0bc66181f8c44759e0c819e9a8da9d75b75a7
Glynn Forrest <me@glynnforrest.com>
2016-04-20 14:29:04 +0100

    Add notification publisher service tag

--- src/NotificationBundle/AdminNotificationBundle.php
+++ src/NotificationBundle/AdminNotificationBundle.php
@@ -3,7 +3,14 @@
 namespace Admin\NotificationBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Admin\NotificationBundle\DependencyInjection\Compiler\RegisterPublishersPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class AdminNotificationBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new RegisterPublishersPass());
+    }
 }
