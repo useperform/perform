6b47a403e9f8bbf99844b19af4699e7dd30e2c91
Glynn Forrest <me@glynnforrest.com>
2016-04-20 14:19:11 +0100

    Add Mailer as a service and allow it to be configured

--- src/Base/DependencyInjection/AdminBaseExtension.php
+++ src/Base/DependencyInjection/AdminBaseExtension.php
@@ -18,13 +18,36 @@ class AdminBaseExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
+        $this->configureTypeRegistry($container);
+        $this->configureMailer($config, $container);
+    }
+
+    protected function configureTypeRegistry(ContainerBuilder $container)
+    {
         $definition = $container->register('admin_base.type_registry', 'Admin\Base\Type\TypeRegistry');
         $definition->addArgument(new Reference('service_container'));
         $definition->addMethodCall('addType', ['string', 'Admin\Base\Type\StringType']);
         $definition->addMethodCall('addType', ['text', 'Admin\Base\Type\TextType']);
         $definition->addMethodCall('addType', ['datetime', 'Admin\Base\Type\DateTimeType']);
+    }
+
+    protected function configureMailer(array $config, ContainerBuilder $container)
+    {
+        if (!$container->hasParameter('noreply@glynnforrest.com')) {
+            $container->setParameter('admin_base.mailer.from_address', 'noreply@glynnforrest.com');
+        }
+
+        if (!isset($config['mailer']['excluded_domains'])) {
+            return;
+        }
+
+        $definition = $container->getDefinition('admin_base.email.mailer');
+        $definition->addMethodCall('setExcludedDomains', [$config['mailer']['excluded_domains']]);
     }
 }
