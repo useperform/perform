bedc6cd4269a5b51e76726d58f30b1f57ab60293
Glynn Forrest <me@glynnforrest.com>
2016-04-21 14:29:31 +0100

    Add events services

---
+++ src/EventsBundle/DependencyInjection/AdminEventsExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Admin\EventsBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * AdminEventsExtension.
+ **/
+class AdminEventsExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
