4152ba8bc7ec0bff7fd62cf45ac9e1e9b5e0e8a8
Glynn Forrest <me@glynnforrest.com>
2016-04-21 14:10:54 +0100

    Start events bundle

---
+++ src/EventsBundle/AdminEventsBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Admin\EventsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * AdminEventsBundle
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class AdminEventsBundle extends Bundle
+{
+}
