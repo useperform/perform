e83cb5e35d06a0eb6f5615197e3a7fac3fcba4fe
Glynn Forrest <me@glynnforrest.com>
2016-05-02 23:59:24 +0100

    Add twitter factory interface

---
+++ src/TwitterBundle/Factory/FactoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Admin\TwitterBundle\Factory;
+
+/**
+ * FactoryInterface
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+interface FactoryInterface
+{
+    public function getClient();
+}
