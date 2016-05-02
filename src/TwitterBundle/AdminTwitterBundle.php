26692f38d7ea995b901c75cc839b174bd5470886
Glynn Forrest <me@glynnforrest.com>
2016-05-02 23:58:37 +0100

    Start twitter bundle

---
+++ src/TwitterBundle/AdminTwitterBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Admin\TwitterBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * AdminTwitterBundle
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class AdminTwitterBundle extends Bundle
+{
+}
