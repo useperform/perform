cbd4f1d7411988cd79edc6b2713406c63593319b
Glynn Forrest <me@glynnforrest.com>
2016-05-03 00:00:51 +0100

    Add config and service definitions for fetcher

---
+++ src/TwitterBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Admin\TwitterBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('admin_twitter');
+
+        $rootNode
+            ->children()
+                ->scalarNode('screen_name')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                ->end()
+                ->integerNode('cache_ttl')
+                    ->defaultValue(600)
+                ->end()
+                ->enumNode('credentials_source')
+                    ->values(['config', 'settings'])
+                    ->defaultValue('config')
+                ->end()
+                ->arrayNode('credentials')
+                    ->children()
+                        ->scalarNode('consumer_key')->cannotBeEmpty()->end()
+                        ->scalarNode('consumer_secret')->cannotBeEmpty()->end()
+                        ->scalarNode('access_token')->cannotBeEmpty()->end()
+                        ->scalarNode('access_token_secret')->cannotBeEmpty()->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
