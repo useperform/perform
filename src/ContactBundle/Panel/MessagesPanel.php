4c990a14cfcd37bf6a3dd950e695d83d0e0e2609
Glynn Forrest <me@glynnforrest.com>
2016-05-26 14:30:26 +0100

    Update MessagesPanel to resolve message entity

--- src/ContactBundle/Panel/MessagesPanel.php
+++ src/ContactBundle/Panel/MessagesPanel.php
@@ -5,6 +5,7 @@ namespace Admin\ContactBundle\Panel;
 use Admin\Base\Panel\PanelInterface;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Admin\Base\Doctrine\EntityResolver;
 
 /**
  * MessagesPanel
@@ -13,18 +14,20 @@ use Doctrine\ORM\EntityManagerInterface;
  **/
 class MessagesPanel implements PanelInterface
 {
+    protected $resolver;
     protected $entityManager;
     protected $templating;
 
-    public function __construct(EntityManagerInterface $entityManager, EngineInterface $templating)
+    public function __construct(EntityResolver $resolver, EntityManagerInterface $entityManager, EngineInterface $templating)
     {
+        $this->resolver = $resolver;
         $this->entityManager = $entityManager;
         $this->templating = $templating;
     }
 
     public function render()
     {
-        $messages = $this->entityManager->getRepository('AdminContactBundle:Message')
+        $messages = $this->entityManager->getRepository($this->resolver->resolve('AdminContactBundle:Message'))
                   ->findBy([], ['createdAt' => 'DESC'], 10);
 
         return $this->templating->render('AdminContactBundle:panels:messages.html.twig', [
