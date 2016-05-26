b28e29de7e62465f7e2a694fe25cf1b471479278
Glynn Forrest <me@glynnforrest.com>
2016-05-26 14:15:53 +0100

    Add a simple admins data collector

---
+++ src/Base/DataCollector/AdminsDataCollector.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Admin\Base\DataCollector;
+
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Admin\Base\Admin\AdminRegistry;
+
+/**
+ * AdminsDataCollector.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class AdminsDataCollector extends DataCollector
+{
+    protected $registry;
+
+    public function __construct(AdminRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+        $admins = $this->registry->getAdmins();
+        foreach ($admins as $entity => $class) {
+            if (strpos($entity, '\\') !== false) {
+                unset($admins[$entity]);
+            }
+        }
+
+        ksort($admins);
+        $this->data = [
+            'admins' => $admins,
+        ];
+        if ($request->attributes->has('_entity')) {
+            $this->data['activeEntity'] = $request->attributes->get('_entity');
+            $this->data['activeAdmin'] = get_class($this->registry->getAdmin($this->data['activeEntity']));
+        }
+    }
+
+    public function getAdmins()
+    {
+        return $this->data['admins'];
+    }
+
+    public function getActiveEntity()
+    {
+        return isset($this->data['activeEntity']) ? $this->data['activeEntity'] : null;
+    }
+
+    public function getActiveAdmin()
+    {
+        return isset($this->data['activeAdmin']) ? $this->data['activeAdmin'] : null;
+    }
+
+    public function getName()
+    {
+        return 'admin_base.admins';
+    }
+}
