694b220d33945c5e404d64e2d9f3fdba31a1a7e1
Glynn Forrest <me@glynnforrest.com>
2016-06-02 20:58:54 +0100

    Add twig extension to get recent blog posts

---
+++ src/BlogBundle/Twig/Extension/BlogExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Admin\BlogBundle\Twig\Extension;
+
+use Symfony\Component\Form\FormFactoryInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * BlogExtension
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class BlogExtension extends \Twig_Extension
+{
+    protected $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('recentBlogPosts', [$this, 'getPosts']),
+        ];
+    }
+
+    public function getPosts($limit = 5)
+    {
+        return $this->entityManager
+            ->getRepository('AdminBlogBundle:Post')
+            ->findRecent($limit);
+    }
+
+    public function getName()
+    {
+        return 'blog';
+    }
+}
