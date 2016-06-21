3feb865efc2ad878aff94eaed9960a170081ba15
Glynn Forrest <me@glynnforrest.com>
2016-06-21 20:14:51 +0100

    Start analytics bundle

---
+++ src/AnalyticsBundle/AdminAnalyticsBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Admin\AnalyticsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * AdminAnalyticsBundle
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class AdminAnalyticsBundle extends Bundle
+{
+}
