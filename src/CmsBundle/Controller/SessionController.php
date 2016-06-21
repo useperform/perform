a4ccd0261e1d42e439b32bc8fa6640f91778f0e4
Glynn Forrest <me@glynnforrest.com>
2016-06-21 14:01:05 +0100

    Add routes to begin and end a cms session

---
+++ src/CmsBundle/Controller/SessionController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Admin\CmsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Admin\CmsBundle\EventListener\ToolbarListener;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * SessionController.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class SessionController extends Controller
+{
+    /**
+     * @Route("/begin")
+     */
+    public function beginAction(Request $request)
+    {
+        $request->getSession()->set(ToolbarListener::SESSION_KEY, true);
+        //configure the entry point
+        return $this->redirect('/');
+    }
+
+    /**
+     * @Route("/end")
+     */
+    public function endAction(Request $request)
+    {
+        $request->getSession()->remove(ToolbarListener::SESSION_KEY);
+        //configure the exit point
+        return $this->redirect('/admin');
+    }
+}
