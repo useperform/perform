5e1eaf69ad19196035929a0e700173c7faf7610a
Glynn Forrest <me@glynnforrest.com>
2016-06-22 18:53:33 +0100

    Add simple interface for a cms block

---
+++ src/CmsBundle/Block/BlockTypeInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Admin\CmsBundle\Block;
+
+use Admin\CmsBundle\Entity\Block;
+
+/**
+ * BlockTypeInterface
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+interface BlockTypeInterface
+{
+    /**
+     * Transform a block entity of this type into content.
+     *
+     * @param Block
+     *
+     * @return string
+     */
+    public function render(Block $block);
+}
