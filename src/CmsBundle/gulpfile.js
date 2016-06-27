d2dba19055a65a7b2172c9cfac2980688e7951e0
Glynn Forrest <me@glynnforrest.com>
2016-06-27 13:35:58 +0100

    Setup gulp for cms javascript

--- src/CmsBundle/gulpfile.js
+++ src/CmsBundle/gulpfile.js
@@ -1,23 +1,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var concat = require('gulp-concat');
 
-function handleError (err) {
-  console.log(err);
-  this.emit('end');
-}
+gulp.task('sass', function (cb) {
+  pump([
+    gulp.src('Resources/scss/app.scss'),
+    sass(),
+    gulp.dest('Resources/public/css'),
+  ], cb);
+});
 
-gulp.task('sass', function () {
-  return gulp.src('Resources/scss/app.scss')
-    .pipe(sass())
-    .on('error', handleError)
-    .pipe(gulp.dest('Resources/public/css'));
+gulp.task('js', function (cb) {
+  pump([
+    gulp.src([
+      'Resources/js/blocks.js',
+      'Resources/js/*.block.js',
+      'Resources/js/app.js',
+    ]),
+    concat('app.js'),
+    // uglify(),
+    gulp.dest('Resources/public/js')
+  ], cb);
 });
 
 gulp.task('watch', function () {
   gulp.watch('Resources/scss/*.scss', ['sass']);
   gulp.watch('../Base/Resources/scss/*.scss', ['sass']);
+  gulp.watch('Resources/js/*.js', ['js']);
 });
 
-gulp.task('develop', ['watch', 'sass']);
 gulp.task('build', ['sass']);
-gulp.task('default', ['develop']);
+gulp.task('default', ['sass', 'js', 'watch']);
