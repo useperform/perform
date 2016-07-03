38f33b5148f234b6888a11ac1aa3bda7e987c4db
Glynn Forrest <me@glynnforrest.com>
2016-07-03 16:06:24 +0100

    Page annotation can take an array of sections

--- src/CmsBundle/Annotation/Page.php
+++ src/CmsBundle/Annotation/Page.php
@@ -13,19 +13,31 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ConfigurationInterface;
 class Page implements ConfigurationInterface
 {
     protected $page;
+    protected $sections;
 
     public function __construct(array $configuration)
     {
-        if (!isset($configuration['value'])) {
-            throw new \InvalidArgumentException(__CLASS__.' requires a page name.');
+        $message = __CLASS__.' requires a page name as a string, with optional sections as an array (e.g. @Page("home", sections={"main", "aside"})).';
+        if (!isset($configuration['value']) || !is_string($configuration['value'])) {
+            throw new \InvalidArgumentException($message);
+        }
+        $sections = isset($configuration['sections']) ? $configuration['sections'] : [];
+        if (!is_array($sections)) {
+            throw new \InvalidArgumentException($message);
         }
 
         $this->page = $configuration['value'];
+        $this->sections = $sections;
     }
 
     public function getPage()
     {
         return $this->page;
+    }
+
+    public function getSections()
+    {
+        return $this->sections;
     }
 
     public function getAliasName()
