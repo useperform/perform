bf172ac3f077406e1b4db1e30f81d9b7e798204e
Glynn Forrest <me@glynnforrest.com>
2016-08-12 15:07:13 +0100

    Add BlockTypeRegistry

---
+++ src/CmsBundle/Block/BlockTypeRegistry.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Admin\CmsBundle\Block;
+
+use Admin\CmsBundle\Exception\BlockTypeNotFoundException;
+
+/**
+ * BlockTypeRegistry
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class BlockTypeRegistry
+{
+    protected $types = [];
+
+    /**
+     * @param string $name
+     * @param BlockTypeInterface $type
+     */
+    public function addType($name, BlockTypeInterface $type)
+    {
+        $this->types[$name] = $type;
+    }
+
+    /**
+     * @return BlockTypeInterface
+     */
+    public function getType($name)
+    {
+        if (!isset($this->types[$name])) {
+            throw new BlockTypeNotFoundException(sprintf('Block type "%s" not found.', $name));
+        }
+
+        return $this->types[$name];
+    }
+}
---
+++ src/CmsBundle/Tests/Block/BlockTypeRegistryTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Admin\CmsBundle\Tests\Block;
+
+use Admin\CmsBundle\Block\BlockTypeRegistry;
+use Admin\CmsBundle\Block\BlockTypeInterface;
+use Admin\CmsBundle\Exception\BlockTypeNotFoundException;
+
+/**
+ * BlockTypeRegistryTest
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class BlockTypeRegistryTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->registry = new BlockTypeRegistry();
+    }
+
+    public function testGetType()
+    {
+        $type = $this->getMock(BlockTypeInterface::class);
+        $this->registry->addType('foo', $type);
+        $this->assertSame($type, $this->registry->getType('foo'));
+    }
+
+    public function testGetUnknownType()
+    {
+        $this->setExpectedException(BlockTypeNotFoundException::class);
+        $this->registry->getType('foo');
+    }
+}
