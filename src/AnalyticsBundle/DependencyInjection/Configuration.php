962dd97b06ebe9547e419d25e1a1e9fdb61ca415
Glynn Forrest <me@glynnforrest.com>
2016-09-24 08:59:27 +0100

    Add configuration for analytics vendors

---
+++ src/AnalyticsBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Perform\AnalyticsBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+/**
+ * Configuration
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('perform_analytics');
+
+        $rootNode
+            ->children()
+                ->arrayNode('vendors')
+                    ->prototype('scalar')->end()
+                ->end()
+            ->end()
+            ;
+
+        return $treeBuilder;
+    }
+}
--- src/AnalyticsBundle/DependencyInjection/PerformAnalyticsExtension.php
+++ src/AnalyticsBundle/DependencyInjection/PerformAnalyticsExtension.php
@@ -17,7 +17,13 @@ class PerformAnalyticsExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $twigExtension = $container->getDefinition('perform_analytics.twig.analytics');
+        $twigExtension->addArgument($config['vendors']);
     }
 }
