4b4c56c7cdfd02d425cf539d78e858b56c27df50
Glynn Forrest <me@glynnforrest.com>
2016-10-14 15:07:18 +0100

    Begin music bundle

---
+++ src/MusicBundle/PerformMusicBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Perform\MusicBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * PerformMusicBundle
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class PerformMusicBundle extends Bundle
+{
+}
