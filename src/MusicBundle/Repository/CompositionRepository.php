5d2a14b2f26b6bbc7ee34cb15bf64566c4448e47
Glynn Forrest <me@glynnforrest.com>
2016-10-15 15:21:32 +0100

    Find compositions nested by category

--- src/MusicBundle/Repository/CompositionRepository.php
+++ src/MusicBundle/Repository/CompositionRepository.php
@@ -27,4 +27,26 @@ class CompositionRepository extends EntityRepository
 
         return $nested;
     }
+
+    public function findNestedByCategory()
+    {
+        $results = $this->findBy([], ['publishDate' => 'DESC']);
+        $nested = [];
+        foreach ($results as $result) {
+            $cat = $result->getCategory();
+            if (!$cat) {
+                continue;
+            }
+
+            if (!isset($nested[$cat])) {
+                $nested[$cat] = [];
+            }
+
+            $nested[$cat][] = $result;
+        }
+
+        ksort($nested);
+
+        return $nested;
+    }
 }
