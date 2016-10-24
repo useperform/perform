b015f56a0236941ad1d16c68b9696712447c81c7
Glynn Forrest <me@glynnforrest.com>
2016-10-24 18:57:07 +0100

    Add twig helper to get blog tags

--- src/BlogBundle/Twig/Extension/BlogExtension.php
+++ src/BlogBundle/Twig/Extension/BlogExtension.php
@@ -23,6 +23,7 @@ class BlogExtension extends \Twig_Extension
     {
         return [
             new \Twig_SimpleFunction('perform_recent_blog_posts', [$this, 'getPosts']),
+            new \Twig_SimpleFunction('perform_blog_published_tags', [$this, 'getTags']),
         ];
     }
 
@@ -31,6 +32,13 @@ class BlogExtension extends \Twig_Extension
         return $this->entityManager
             ->getRepository('PerformBlogBundle:Post')
             ->findRecent($limit);
+    }
+
+    public function getTags()
+    {
+        return $this->entityManager
+            ->getRepository('PerformBlogBundle:Tag')
+            ->findPublished();
     }
 
     public function getName()
