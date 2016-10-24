fdcd67bb5d2b949b716a11f5768438b29d300eab
Glynn Forrest <me@glynnforrest.com>
2016-10-24 18:09:24 +0100

    Add tags to post fixtures

--- src/BlogBundle/DataFixtures/ORM/LoadPostData.php
+++ src/BlogBundle/DataFixtures/ORM/LoadPostData.php
@@ -6,6 +6,7 @@ use Faker;
 use Doctrine\Common\Persistence\ObjectManager;
 use Perform\BlogBundle\Entity\Post;
 use Perform\BaseBundle\DataFixtures\ORM\EntityDeclaringFixtureInterface;
+use Perform\BlogBundle\Entity\Tag;
 
 /**
  * LoadTeamMemberData.
@@ -17,12 +18,30 @@ class LoadPostData implements EntityDeclaringFixtureInterface
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create();
+        $tags = [];
+        foreach (['Music',
+                  'Performances',
+                  'Members',
+                  'Funding',
+                  'Teaching',
+                  'Tour',
+        ] as $name) {
+            $tag = new Tag();
+            $tag->setName($name);
+            $tags[] = $tag;
+        }
+
         for ($i = 0; $i < 10; ++$i) {
             $post = new Post();
             $post->setTitle($faker->sentence);
             $post->setPublishDate($faker->dateTimeThisYear);
             $post->setContent(implode("\n\n", $faker->paragraphs(5)));
             $post->setEnabled(rand(0, 2) ? true : false);
+            $tagAmount = rand(0, 5);
+            for ($j = 0; $j < $tagAmount; $j++) {
+                $post->addTag($tags[array_rand($tags)]);
+            }
+
             $manager->persist($post);
         }
         $manager->flush();
