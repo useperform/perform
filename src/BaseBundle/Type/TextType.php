4b93b296428270063b9efa84abfb21bbd8a82dae
Glynn Forrest <me@glynnforrest.com>
2016-10-25 16:57:23 +0100

    TextType returns html and shows newlines

--- src/BaseBundle/Type/TextType.php
+++ src/BaseBundle/Type/TextType.php
@@ -4,6 +4,7 @@ namespace Perform\BaseBundle\Type;
 
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Perform\BaseBundle\Type\TypeConfig;
 
 /**
  * TextType
@@ -15,5 +16,19 @@ class TextType extends AbstractType
     public function createContext(FormBuilderInterface $builder, $field, array $options = [])
     {
         $builder->add($field, TextareaType::class);
+    }
+
+    public function listContext($entity, $field, array $options = [])
+    {
+        //escape the content, then run it through nl2br
+        return nl2br(htmlspecialchars($this->accessor->getValue($entity, $field)));
+    }
+
+    public function getHtmlContexts()
+    {
+        return [
+            TypeConfig::CONTEXT_LIST,
+            TypeConfig::CONTEXT_VIEW,
+        ];
     }
 }
