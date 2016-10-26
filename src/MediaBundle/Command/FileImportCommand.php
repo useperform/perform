2e4f9c96ad1e8bb7faced0729b901c7d10d25b3a
Glynn Forrest <me@glynnforrest.com>
2016-10-26 13:44:21 +0100

    Allow multiple paths and directories to be imported

--- src/MediaBundle/Command/FileImportCommand.php
+++ src/MediaBundle/Command/FileImportCommand.php
@@ -5,12 +5,11 @@ namespace Perform\MediaBundle\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
-use RecursiveIteratorIterator;
-use RecursiveDirectoryIterator;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Finder\Finder;
 
 /**
- * FileImportCommand
+ * FileImportCommand.
  *
  * @author Glynn Forrest <me@glynnforrest.com>
  */
@@ -24,31 +23,45 @@ class FileImportCommand extends ContainerAwareCommand
         $this->setName($this->name)
              ->setDescription($this->description)
              ->addArgument(
-                 'path',
-                 InputArgument::REQUIRED,
-                 'The path to the file or directory'
+                 'paths',
+                 InputArgument::IS_ARRAY,
+                 'The paths to files or directories'
              );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $importer = $this->getContainer()->get('perform_media.importer.file');
-        foreach ($this->getFiles($input->getArgument('path')) as $file) {
-            $importer->import($file->getPathname());
-            $output->writeln(sprintf('Imported <info>%s</info>', $file->getPathname()));
+        foreach ($this->getFiles($input->getArgument('paths')) as $path) {
+            $importer->import($path);
+            $output->writeln(sprintf('Imported <info>%s</info>', $path));
         }
     }
 
-    protected function getFiles($path)
+    protected function getFiles(array $paths)
     {
-        if (!is_dir($path)) {
-            return [new \SplFileObject($path)];
+        $files = [];
+        $finder = new Finder();
+        $found = [];
+        $dirs = 0;
+
+        foreach ($paths as $path) {
+            if (is_file($path)) {
+                $files[] = $path;
+                continue;
+            }
+
+            $finder->in($path);
+            $dirs++;
         }
 
-        return new RecursiveIteratorIterator(
-            new RecursiveDirectoryIterator(
-                $path, RecursiveDirectoryIterator::SKIP_DOTS
-            )
-        );
+        //finder will error out if in() hasn't been called
+        if ($dirs > 0) {
+            $found = array_map(function ($file) {
+                return $file->getRealPath();
+            }, array_values(iterator_to_array($finder->files())));
+        }
+
+        return array_unique(array_merge($files, $found));
     }
 }
