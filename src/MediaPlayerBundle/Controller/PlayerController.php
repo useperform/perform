44a7a52d02c3527d441e0b40e7d79fbc830b4ccd
Glynn Forrest <me@glynnforrest.com>
2016-10-28 18:47:28 +0100

    Get a playlist and its items

--- src/MediaPlayerBundle/Controller/PlayerController.php
+++ src/MediaPlayerBundle/Controller/PlayerController.php
@@ -5,6 +5,8 @@ namespace Perform\MediaPlayerBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Perform\MediaPlayerBundle\Entity\Playlist;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * PlayerController
@@ -20,5 +22,26 @@ class PlayerController extends Controller
     public function showAction()
     {
         return [];
+    }
+
+    /**
+     * @Route("/playlist/{id}")
+     */
+    public function playlistAction(Playlist $playlist)
+    {
+        $data = [
+            'playlist' => [
+                'id' => $playlist->getId(),
+                'title' => $playlist->getTitle(),
+            ],
+            'items' => []
+        ];
+        foreach ($playlist->getItems() as $item) {
+            $data['items'][] = [
+                'url' => $item->getFile()->getFileName(),
+            ];
+        }
+
+        return new JsonResponse($data);
     }
 }
