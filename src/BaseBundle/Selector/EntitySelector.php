9b58161c7c4df5956b5a7ba745c97b654ed4a17e
Glynn Forrest <me@glynnforrest.com>
2016-10-30 15:37:34 +0000

    EntitySelector only sorts the column if the field is configured to

--- src/BaseBundle/Selector/EntitySelector.php
+++ src/BaseBundle/Selector/EntitySelector.php
@@ -7,6 +7,9 @@ use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Symfony\Component\HttpFoundation\Request;
 use Perform\BaseBundle\Admin\AdminRegistry;
 use Doctrine\ORM\EntityManagerInterface;
+use Perform\BaseBundle\Type\EntityTypeConfig;
+use Doctrine\ORM\QueryBuilder;
+use Perform\BaseBundle\Type\TypeConfig;
 
 /**
  * EntitySelector
@@ -16,12 +19,14 @@ use Doctrine\ORM\EntityManagerInterface;
 class EntitySelector
 {
     protected $entityManager;
+    protected $typeConfig;
     protected $registry;
 
-    public function __construct(EntityManagerInterface $entityManager, AdminRegistry $registry)
+    public function __construct(EntityManagerInterface $entityManager, AdminRegistry $registry, EntityTypeConfig $typeConfig)
     {
         $this->entityManager = $entityManager;
         $this->registry = $registry;
+        $this->typeConfig = $typeConfig;
     }
 
     public function listContext(Request $request, $entityName)
@@ -29,13 +34,17 @@ class EntitySelector
         $qb = $this->entityManager->createQueryBuilder()
             ->select('e')
             ->from($entityName, 'e');
+
+        $orderField = $request->query->get('sort', null);
         $direction = strtoupper($request->query->get('direction', 'asc'));
         if ($direction !== 'DESC') {
             $direction = 'ASC';
         }
-        if ($orderField = $request->query->get('sort', null)) {
-            $qb->orderBy('e.'.$orderField, $direction);
-        }
+        $this->maybeOrderBy($qb, $entityName, $orderField, $direction);
+
+        //pass the builder into the admin for potential filtering, or even
+        //creating a new builder entirely
+
         $paginator = new Pagerfanta(new DoctrineORMAdapter($qb));
         $paginator->setMaxPerPage(10);
         $paginator->setCurrentPage($request->query->get('page', 1));
@@ -46,5 +55,17 @@ class EntitySelector
         ];
 
         return [$paginator, $orderBy];
+    }
+
+    protected function maybeOrderBy(QueryBuilder $qb, $entityName, $orderField, $direction)
+    {
+        if (!$orderField) {
+            return;
+        }
+
+        $typeConfig = $this->typeConfig->getEntityTypeConfig($entityName)->getTypes(TypeConfig::CONTEXT_LIST);
+        if (isset($typeConfig[$orderField]['options']['sort']) && $typeConfig[$orderField]['options']['sort'] === true) {
+            $qb->orderBy('e.'.$orderField, $direction);
+        }
     }
 }
--- src/BaseBundle/Tests/Selector/EntitySelectorTest.php
+++ src/BaseBundle/Tests/Selector/EntitySelectorTest.php
@@ -5,6 +5,7 @@ namespace Perform\BaseBundle\Tests\Selector;
 use Perform\BaseBundle\Selector\EntitySelector;
 use Symfony\Component\HttpFoundation\Request;
 use Pagerfanta\Pagerfanta;
+use Perform\BaseBundle\Type\TypeConfig;
 
 /**
  * EntitySelectorTest
@@ -16,31 +17,100 @@ class EntitySelectorTest extends \PHPUnit_Framework_TestCase
     protected $entityManager;
     protected $registry;
     protected $selector;
+    protected $qb;
+    protected $typeConfig;
 
     public function setUp()
     {
         $this->entityManager = $this->getMock('Doctrine\ORM\EntityManagerInterface');
-        $qb = $this->getMockBuilder('Doctrine\ORM\QueryBuilder')
+        $this->qb = $this->getMockBuilder('Doctrine\ORM\QueryBuilder')
             ->disableOriginalConstructor()
-            ->setMethods(['from'])
             ->getMock();
 
         $this->entityManager->expects($this->any())
             ->method('createQueryBuilder')
-            ->will($this->returnValue($qb));
+            ->will($this->returnValue($this->qb));
 
         $this->registry = $this->getMockBuilder('Perform\BaseBundle\Admin\AdminRegistry')
                         ->disableOriginalConstructor()
                         ->getMock();
-        $this->selector = new EntitySelector($this->entityManager, $this->registry);
+        $this->typeConfig = $this->getMockBuilder('Perform\BaseBundle\Type\EntityTypeConfig')
+                          ->disableOriginalConstructor()
+                          ->getMock();
+        $this->selector = new EntitySelector($this->entityManager, $this->registry, $this->typeConfig);
+    }
+
+    protected function expectQueryBuilder($entityName)
+    {
+        $this->qb->expects($this->once())
+            ->method('select')
+            ->with('e')
+            ->will($this->returnSelf());
+        $this->qb->expects($this->once())
+            ->method('from')
+            ->with($entityName, 'e')
+            ->will($this->returnSelf());
+    }
+
+    protected function expectTypeConfig($entityName, array $config)
+    {
+        $typeConfig = new TypeConfig();
+        foreach ($config as $field => $config) {
+            $typeConfig->add($field, $config);
+        }
+
+        $this->typeConfig->expects($this->once())
+            ->method('getEntityTypeConfig')
+            ->with($entityName)
+            ->will($this->returnValue($typeConfig));
     }
 
     public function testDefaultListContext()
     {
-        $request = new Request;
-        $result = $this->selector->listContext($request, 'PerformBaseBundle:User');
+        $this->expectQueryBuilder('Bundle:SomeEntity');
+        $result = $this->selector->listContext(new Request, 'Bundle:SomeEntity');
+
         $this->assertInternalType('array', $result);
         $this->assertInstanceOf(Pagerfanta::class, $result[0]);
         $this->assertInternalType('array', $result[1]);
+    }
+
+    public function testListContextWithSorting()
+    {
+        $request = new Request;
+        $request->query->set('sort', 'title');
+        $request->query->set('direction', 'DESC');
+
+        $this->expectQueryBuilder('Bundle:SomeEntity');
+        $this->expectTypeConfig('Bundle:SomeEntity', [
+            'title' => [
+                'type' => 'string',
+                'sort' => true,
+            ]
+        ]);
+        $this->qb->expects($this->once())
+            ->method('orderBy')
+            ->with('e.title', 'DESC');
+
+        $this->selector->listContext($request, 'Bundle:SomeEntity');
+    }
+
+    public function testListContextWithDisabledSortField()
+    {
+        $request = new Request;
+        $request->query->set('sort', 'enabled');
+        $request->query->set('direction', 'DESC');
+
+        $this->expectQueryBuilder('Bundle:SomeEntity');
+        $this->expectTypeConfig('Bundle:SomeEntity', [
+            'enabled' => [
+                'type' => 'boolean',
+                'sort' => false,
+            ]
+        ]);
+        $this->qb->expects($this->never())
+            ->method('orderBy');
+
+        $this->selector->listContext($request, 'Bundle:SomeEntity');
     }
 }
