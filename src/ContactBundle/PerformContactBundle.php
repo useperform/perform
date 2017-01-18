926a740ccc8589c5bb741c5708d24c56c34b504f
Glynn Forrest <me@glynnforrest.com>
2017-01-18 14:06:52 +0000

    Register honeypot form template automatically

--- src/ContactBundle/PerformContactBundle.php
+++ src/ContactBundle/PerformContactBundle.php
@@ -3,12 +3,19 @@
 namespace Perform\ContactBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Perform\ContactBundle\DependencyInjection\Compiler\FormTemplatePass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
- * PerformContactBundle
+ * PerformContactBundle.
  *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
 class PerformContactBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new FormTemplatePass());
+    }
 }
