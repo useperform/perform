e146dffd33668563bcc4ec86bc2c14e043eba91a
Glynn Forrest <me@glynnforrest.com>
2017-01-26 17:52:55 +0000

    Move twig filter to util extension

---
+++ src/BaseBundle/Twig/Extension/UtilExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Perform\BaseBundle\Twig\Extension;
+
+use Carbon\Carbon;
+
+/**
+ * General twig helpers.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class UtilExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('human_date', [$this, 'humanDate']),
+        ];
+    }
+
+    public function humanDate(\DateTime $date = null)
+    {
+        if (!$date) {
+            return '';
+        }
+        return Carbon::instance($date)->diffForHumans();
+    }
+
+
+    public function getName()
+    {
+        return 'perform_base_util';
+    }
+}
