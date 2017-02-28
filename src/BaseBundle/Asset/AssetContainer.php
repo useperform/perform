33dc3a0e93ce1596339b2537801cda12c2ab5e6c
Glynn Forrest <me@glynnforrest.com>
2017-02-28 12:59:03 +0000

    assets: create AssetContainer to dynamically add assets
    Over the course of a request, crud types may wish to inject js
    dynamically into the page.

---
+++ src/BaseBundle/Asset/AssetContainer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Perform\BaseBundle\Asset;
+
+/**
+ * Store a list of css and js assets over the course of a request.
+ *
+ * Render these assets later using the assets twig extension.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class AssetContainer
+{
+    protected $js = [];
+    protected $css = [];
+
+    public function addJs($src)
+    {
+        if (!in_array($src, $this->js)) {
+            $this->js[] = $src;
+        }
+    }
+
+    public function getJs()
+    {
+        return $this->js;
+    }
+
+    public function addCss($src)
+    {
+        if (!in_array($src, $this->css)) {
+            $this->css[] = $src;
+        }
+    }
+
+    public function getCss()
+    {
+        return $this->css;
+    }
+}
---
+++ src/BaseBundle/Tests/Asset/AssetContainerTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Perform\BaseBundle\Tests\Asset;
+
+use Perform\BaseBundle\Asset\AssetContainer;
+
+/**
+ * AssetContainerTest
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class AssetContainerTest extends \PHPUnit_Framework_TestCase
+{
+    protected $assets;
+
+    public function setUp()
+    {
+        $this->assets = new AssetContainer();
+    }
+
+    public function testJs()
+    {
+        $this->assets->addJs('/js/foo.js');
+        $this->assets->addJs('/js/bar.js');
+
+        $this->assertSame(['/js/foo.js', '/js/bar.js'], $this->assets->getJs());
+    }
+
+    public function testDuplicateJs()
+    {
+        $this->assets->addJs('/js/foo.js');
+        $this->assets->addJs('/js/bar.js');
+        $this->assets->addJs('/js/foo.js');
+        $this->assets->addJs('/js/foo.js');
+
+        $this->assertSame(['/js/foo.js', '/js/bar.js'], $this->assets->getJs());
+    }
+
+    public function testCss()
+    {
+        $this->assets->addCss('/css/foo.css');
+        $this->assets->addCss('/css/bar.css');
+
+        $this->assertSame(['/css/foo.css', '/css/bar.css'], $this->assets->getCss());
+    }
+
+    public function testDuplicateCss()
+    {
+        $this->assets->addCss('/css/foo.css');
+        $this->assets->addCss('/css/bar.css');
+        $this->assets->addCss('/css/foo.css');
+        $this->assets->addCss('/css/foo.css');
+
+        $this->assertSame(['/css/foo.css', '/css/bar.css'], $this->assets->getCss());
+    }
+}
