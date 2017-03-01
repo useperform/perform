002ec6a5f255f9fca79bfa2d281d9b50e63b9105
Glynn Forrest <me@glynnforrest.com>
2017-03-01 19:04:56 +0000

    types: add format option for DurationType

--- src/BaseBundle/Type/DurationType.php
+++ src/BaseBundle/Type/DurationType.php
@@ -13,9 +13,25 @@ use Perform\BaseBundle\Util\DurationUtil;
  **/
 class DurationType extends AbstractType
 {
+    const FORMAT_DIGITAL = 0;
+    const FORMAT_HUMAN = 1;
+    const FORMAT_VERBOSE = 2;
+
     public function listContext($entity, $field, array $options = [])
     {
-        return DurationUtil::toHuman($this->accessor->getValue($entity, $field));
+        $duration = $this->accessor->getValue($entity, $field);
+
+        switch ($options['format']) {
+        case static::FORMAT_DIGITAL:
+            return DurationUtil::toDigital($duration);
+        case static::FORMAT_HUMAN:
+            return DurationUtil::toHuman($duration);
+        case static::FORMAT_VERBOSE:
+            return DurationUtil::toVerbose($duration);
+        default:
+            throw new \InvalidArgumentException(sprintf('Invalid "format" option passed to "%s"', __CLASS__));
+        }
+
     }
 
     public function createContext(FormBuilderInterface $builder, $field, array $options = [])
@@ -26,5 +42,17 @@ class DurationType extends AbstractType
     public function getTemplate()
     {
         return 'PerformBaseBundle:types:duration.html.twig';
+    }
+
+    public function getDefaultConfig()
+    {
+        return [
+            'listOptions' => [
+                'format' => DurationType::FORMAT_DIGITAL,
+            ],
+            'viewOptions' => [
+                'format' => DurationType::FORMAT_VERBOSE,
+            ]
+        ];
     }
 }
