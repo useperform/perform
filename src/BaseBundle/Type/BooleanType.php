83a8f6ee8a23b0de5419e841ff905e77a20690b4
Glynn Forrest <me@glynnforrest.com>
2017-03-02 12:58:03 +0000

    types: make labels of boolean values configurable

--- src/BaseBundle/Type/BooleanType.php
+++ src/BaseBundle/Type/BooleanType.php
@@ -14,11 +14,25 @@ class BooleanType extends AbstractType
 {
     public function listContext($entity, $field, array $options = [])
     {
-        return $this->accessor->getValue($entity, $field) ? 'Yes' : 'No';
+        $labels = $options['valueLabels'];
+        if (count($labels) !== 2) {
+            throw new \InvalidArgumentException(sprintf('%s expects the "valueLabels" option to be an array with 2 values.', __CLASS__));
+        }
+
+        return $this->accessor->getValue($entity, $field) ? $labels[0] : $labels[1];
     }
 
     public function createContext(FormBuilderInterface $builder, $field, array $options = [])
     {
         $builder->add($field, CheckboxType::class);
+    }
+
+    public function getDefaultConfig()
+    {
+        return [
+            'options' => [
+                'valueLabels' => ['Yes', 'No'],
+            ]
+        ];
     }
 }
