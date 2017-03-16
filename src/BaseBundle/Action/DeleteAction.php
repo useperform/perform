e3b2e0ac7f73e8a266079edf6547123b2b66fa5c
Glynn Forrest <me@glynnforrest.com>
2017-03-16 14:18:46 +0000

    actions: add simple delete action

---
+++ src/BaseBundle/Action/DeleteAction.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Perform\BaseBundle\Action;
+
+use Doctrine\ORM\EntityManager;
+
+/**
+ * DeleteAction.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class DeleteAction implements ActionInterface
+{
+    protected $entityManager;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function run(array $entities, array $options)
+    {
+        foreach ($entities as $entity) {
+            $this->entityManager->remove($entity);
+        }
+
+        $this->entityManager->flush();
+
+        $response = new ActionResponse(sprintf('%s deleted.', count($entities) === 1 ? 'Item' : count($entities).' items'));
+        $response->setRedirect(ActionResponse::REDIRECT_CURRENT);
+
+        return $response;
+    }
+
+    public function isGranted($message)
+    {
+        return true;
+    }
+
+    public function getLabel($message)
+    {
+    }
+}
