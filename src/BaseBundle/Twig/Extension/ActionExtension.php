6ef343f4b5061b69f92cacbf9a99b57da63da145
Glynn Forrest <me@glynnforrest.com>
2017-03-21 00:08:12 +0000

    actions: twig function to render a batch action option

--- src/BaseBundle/Twig/Extension/ActionExtension.php
+++ src/BaseBundle/Twig/Extension/ActionExtension.php
@@ -26,6 +26,7 @@ class ActionExtension extends \Twig_Extension
     {
         return [
             new \Twig_SimpleFunction('perform_action_button', [$this, 'actionButton'], ['is_safe' => ['html'], 'needs_environment' => true]),
+            new \Twig_SimpleFunction('perform_action_option', [$this, 'actionOption'], ['is_safe' => ['html'], 'needs_environment' => true]),
             new \Twig_SimpleFunction('perform_action_is_granted', [$this, 'isActionGranted']),
             new \Twig_SimpleFunction('perform_action_for_entity', [$this->registry, 'getActionsForEntity']),
             new \Twig_SimpleFunction('perform_actions_for_class', [$this->registry, 'getActionsForEntityClass']),
@@ -49,6 +50,25 @@ class ActionExtension extends \Twig_Extension
         return $twig->render('PerformBaseBundle:Action:button.html.twig', [
             'label' => $label,
             'attr' => $attr,
+        ]);
+    }
+
+    public function actionOption(\Twig_Environment $twig, ConfiguredAction $action, $entityClass)
+    {
+        $label = $action->getBatchLabel();
+
+        $attr = [];
+        $attr['data-action'] = json_encode([
+            'entityClass' => $entityClass,
+            'label' => $label,
+            //need to change the message depending on the number of entities - ajax?
+            'message' => 'Are you sure you want to do this?',
+            'confirm' => $action->isConfirmationRequired(),
+        ]);
+        $attr['value'] = $this->urlGenerator->generate('perform_base_action_index', ['action' => $action->getName()]);
+        return $twig->render('PerformBaseBundle:Action:option.html.twig', [
+            'attr' => $attr,
+            'label' => $label,
         ]);
     }
 
