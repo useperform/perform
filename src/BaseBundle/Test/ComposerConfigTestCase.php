bdddc04bf49c92e5f0c90bb432fa82d27fde2733
Glynn Forrest <me@glynnforrest.com>
2017-04-01 17:22:24 +0100

    subsplit: extract composer test case for testing other bundles

---
+++ src/BaseBundle/Test/ComposerConfigTestCase.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Perform\BaseBundle\Test;
+
+/**
+ * ComposerConfigTestCase.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+abstract class ComposerConfigTestCase extends \PHPUnit_Framework_TestCase
+{
+    abstract protected function getPackageName();
+
+    abstract protected function getNamespace();
+
+    protected function getBundleConfig()
+    {
+        $bundleDir = dirname((new \ReflectionObject($this))->getFileName());
+
+        $config = json_decode(file_get_contents($bundleDir.'/../composer.json'), true);
+        if ($config === null) {
+            throw new \Exception('Unable to load bundle composer.json: '.json_last_error_msg());
+        }
+
+        return $config;
+    }
+
+    protected function getParentConfig()
+    {
+        return json_decode(file_get_contents(__DIR__.'/../../../composer.json'), true);
+    }
+
+    public function testName()
+    {
+        $this->assertSame($this->getPackageName(), $this->getBundleConfig()['name']);
+    }
+
+    public function testLicense()
+    {
+        $this->assertSame('proprietary', $this->getBundleConfig()['license']);
+    }
+
+    public function testAutoload()
+    {
+        //namespaces must ends with a backslash to be psr4 compatible
+        $namespace = trim($this->getNamespace(), '\\').'\\';
+        $this->assertSame([$namespace => ''], $this->getBundleConfig()['autoload']['psr-4']);
+    }
+
+    public function testReplaceIsDefinedInParentConfig()
+    {
+        $parentConfig = $this->getParentConfig();
+        $name = $this->getPackageName();
+        $this->assertArrayHasKey($name, $parentConfig['replace'], sprintf('Parent composer.json does not replace bundle "%s"', $name));
+        $this->assertSame('self.version', $parentConfig['replace'][$name], 'Parent composer.json must use self.version when replacing bundles');
+    }
+
+    public function testRequireVersionsMatch()
+    {
+        $bundleConfig = $this->getBundleConfig();
+        $parentConfig = $this->getParentConfig();
+
+        $bundleDeps = isset($bundleConfig['require']) ? $bundleConfig['require'] : [];
+        $parentDeps = $parentConfig['require'];
+
+        foreach ($bundleDeps as $dep => $version) {
+            $this->assertArrayHasKey($dep, $parentDeps, sprintf('Parent composer.json does not contain dependency "%s"', $dep));
+            $this->assertSame($parentDeps[$dep], $version, sprintf('Required version of "%s" does not match parent composer.json', $dep));
+        }
+    }
+
+    public function testRequireDevVersionsMatch()
+    {
+        $bundleConfig = $this->getBundleConfig();
+        $parentConfig = $this->getParentConfig();
+
+        $bundleDeps = isset($bundleConfig['require-dev']) ? $bundleConfig['require-dev'] : [];
+        $parentDeps = $parentConfig['require-dev'];
+
+        foreach ($bundleDeps as $dep => $version) {
+            $this->assertArrayHasKey($dep, $parentDeps, sprintf('Parent composer.json does not contain dev dependency "%s"', $dep));
+            $this->assertSame($parentDeps[$dep], $version, sprintf('Required dev version of "%s" does not match parent composer.json', $dep));
+        }
+    }
+}
--- src/BaseBundle/Tests/ComposerConfigTest.php
+++ src/BaseBundle/Tests/ComposerConfigTest.php
@@ -2,73 +2,22 @@
 
 namespace Perform\BaseBundle\Tests;
 
+use Perform\BaseBundle\Test\ComposerConfigTestCase;
+
 /**
  * ComposerConfigTest.
  *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
-class ComposerConfigTest extends \PHPUnit_Framework_TestCase
+class ComposerConfigTest extends ComposerConfigTestCase
 {
-    protected function getBundleConfig()
+    protected function getPackageName()
     {
-        return json_decode(file_get_contents(__DIR__.'/../composer.json'), true);
+        return 'perform/base-bundle';
     }
 
-    protected function getParentConfig()
+    protected function getNamespace()
     {
-        return json_decode(file_get_contents(__DIR__.'/../../../composer.json'), true);
-    }
-
-    public function testName()
-    {
-        $this->assertSame('perform/base-bundle', $this->getBundleConfig()['name']);
-    }
-
-    public function testLicense()
-    {
-        $this->assertSame('proprietary', $this->getBundleConfig()['license']);
-    }
-
-    public function testAutoload()
-    {
-        //remove Tests
-        $namespace = substr(__NAMESPACE__, 0, -5);
-        $this->assertSame([$namespace => ''], $this->getBundleConfig()['autoload']['psr-4']);
-    }
-
-    public function testReplaceIsDefinedInParentConfig()
-    {
-        $parentConfig = $this->getParentConfig();
-        $name = 'perform/base-bundle';
-        $this->assertArrayHasKey($name, $parentConfig['replace'], sprintf('Parent composer.json does not replace bundle "%s"', $name));
-        $this->assertSame('self.version', $parentConfig['replace'][$name], 'Parent composer.json must use self.version when replacing bundles');
-    }
-
-    public function testRequireVersionsMatch()
-    {
-        $bundleConfig = $this->getBundleConfig();
-        $parentConfig = $this->getParentConfig();
-
-        $bundleDeps = isset($bundleConfig['require']) ? $bundleConfig['require'] : [];
-        $parentDeps = $parentConfig['require'];
-
-        foreach ($bundleDeps as $dep => $version) {
-            $this->assertArrayHasKey($dep, $parentDeps, sprintf('Parent composer.json does not contain dependency "%s"', $dep));
-            $this->assertSame($parentDeps[$dep], $version, sprintf('Required version of "%s" does not match parent composer.json', $dep));
-        }
-    }
-
-    public function testRequireDevVersionsMatch()
-    {
-        $bundleConfig = $this->getBundleConfig();
-        $parentConfig = $this->getParentConfig();
-
-        $bundleDeps = isset($bundleConfig['require-dev']) ? $bundleConfig['require-dev'] : [];
-        $parentDeps = $parentConfig['require-dev'];
-
-        foreach ($bundleDeps as $dep => $version) {
-            $this->assertArrayHasKey($dep, $parentDeps, sprintf('Parent composer.json does not contain dev dependency "%s"', $dep));
-            $this->assertSame($parentDeps[$dep], $version, sprintf('Required dev version of "%s" does not match parent composer.json', $dep));
-        }
+        return 'Perform\BaseBundle';
     }
 }
