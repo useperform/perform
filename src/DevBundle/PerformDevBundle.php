1e5d291d30ec960f5d10c7745064da0cebee137a
Glynn Forrest <me@glynnforrest.com>
2017-04-20 18:38:30 +0100

    dev: start PerformDevBundle

---
+++ src/DevBundle/PerformDevBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Perform\DevBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * PerformDevBundle.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class PerformDevBundle extends Bundle
+{
+}
