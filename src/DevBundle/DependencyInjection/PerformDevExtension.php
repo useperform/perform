9aa808ca0d875cc106bd000862ef15936f867f91
Glynn Forrest <me@glynnforrest.com>
2017-04-20 18:41:04 +0100

    dev: add file creator as a service

---
+++ src/DevBundle/DependencyInjection/PerformDevExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Perform\DevBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * PerformDevExtension
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class PerformDevExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
