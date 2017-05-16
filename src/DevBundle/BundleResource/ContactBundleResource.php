21fd0d3e86161755553b71cddf9a1cea0a8212fc
Glynn Forrest <me@glynnforrest.com>
2017-05-16 10:59:42 +0100

    dev: Add contact bundle resource

---
+++ src/DevBundle/BundleResource/ContactBundleResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Perform\DevBundle\BundleResource;
+
+use Perform\ContactBundle\PerformContactBundle;
+
+/**
+ * ContactBundleResource
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class ContactBundleResource implements BundleResourceInterface
+{
+    public function getBundleName()
+    {
+        return 'PerformContactBundle';
+    }
+
+    public function getBundleClass()
+    {
+        return PerformContactBundle::class;
+    }
+
+    public function getRequiredBundleClasses()
+    {
+        return [];
+    }
+
+    public function getComposerPackage()
+    {
+        return 'perform/contact-bundle';
+    }
+
+    public function getRoutes()
+    {
+        return '
+perform_contact:
+    resource: "@PerformContactBundle/Resources/config/routing.yml"
+    prefix: /admin/contact
+';
+    }
+}
