71c0e8c6d60a02f732d0ec32c9aa6682face08e6
Glynn Forrest <me@glynnforrest.com>
2017-05-23 15:58:19 +0100

    dev: Start scaffolding frontend interface

---
+++ src/DevBundle/Frontend/FrontendInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Perform\DevBundle\Frontend;
+
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Perform\DevBundle\File\FileCreator;
+
+/**
+ * Used to generate resources for a 'frontend' bundle.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+interface FrontendInterface
+{
+    /**
+     * @return string
+     */
+    public function getName();
+
+    public function createBaseFiles(BundleInterface $bundle, FileCreator $creator);
+}
