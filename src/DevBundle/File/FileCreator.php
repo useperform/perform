c6c99c51f559ebd407d9c953e0844b2796c4d3b3
Glynn Forrest <me@glynnforrest.com>
2017-05-23 17:14:42 +0100

    dev: FileCreator handles all console interaction
    Listens to the command event and injects the environment automatically.

--- src/DevBundle/File/FileCreator.php
+++ src/DevBundle/File/FileCreator.php
@@ -2,9 +2,13 @@
 
 namespace Perform\DevBundle\File;
 
-use Perform\DevBundle\Exception\FileException;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 /**
  * FileCreator.
@@ -15,6 +19,12 @@ class FileCreator
 {
     protected $fs;
     protected $twig;
+    protected $input;
+    protected $output;
+    protected $helperSet;
+
+    const OPTION_SKIP_EXISTING = 'skip-existing';
+    const OPTION_FORCE = 'force';
 
     public function __construct(Filesystem $fs, \Twig_Environment $twig)
     {
@@ -22,23 +32,64 @@ class FileCreator
         $this->twig = $twig;
     }
 
+    public function onConsoleCommand(ConsoleCommandEvent $event)
+    {
+        $input = $event->getInput();
+        $output = $event->getOutput();
+        $helperSet = $event->getCommand()->getHelperSet();
+
+        $this->setConsoleEnvironment($input, $output, $helperSet);
+    }
+
+    public function setConsoleEnvironment(InputInterface $input, OutputInterface $output, HelperSet $helperSet)
+    {
+        $this->input = $input;
+        $this->output = $output;
+        $this->helperSet = $helperSet;
+    }
+
     public function create($file, $contents)
     {
         if ($this->fs->exists($file)) {
-            throw new FileException($file.' exists.');
+            if ($this->input->getOption(static::OPTION_SKIP_EXISTING)) {
+                return;
+            }
+
+            if (!$this->input->getOption(static::OPTION_FORCE)) {
+                $question = new ConfirmationQuestion("<info>$file</info> exists. Overwrite? (y/N) ", false);
+                //add another option - view a diff by creating a temp file and comparing
+                if (!$this->helperSet->get('question')->ask($this->input, $this->output, $question)) {
+                    return;
+                }
+            }
         }
 
-        return $this->forceCreate($file, $contents);
+        $this->forceCreate($file, $contents);
     }
 
     public function forceCreate($file, $contents)
     {
-        return $this->fs->dumpFile($file, $contents);
+        $this->fs->dumpFile($file, $contents);
+        $this->output->writeln(sprintf('Created <info>%s</info>', $file));
+    }
+
+    public function createInBundle(BundleInterface $bundle, $relativeFile, $template, array $vars = [])
+    {
+        $file = $bundle->getPath().'/'.trim($relativeFile, '/');
+
+        return $this->create($file, $this->render($template, $vars));
     }
 
     public function render($template, array $vars = [])
     {
         return $this->twig->render('PerformDevBundle:skeletons:'.$template, $vars);
+    }
+
+    public function createBundleClass(BundleInterface $bundle, $relativeClass, $template, array $vars = [])
+    {
+        list($file, $vars) = $this->resolveBundleClass($bundle, $relativeClass, $vars);
+
+        return $this->create($file, $this->render($template, $vars));
     }
 
     public function resolveBundleClass(BundleInterface $bundle, $relativeClass, array $vars = [])
