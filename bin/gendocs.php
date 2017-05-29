5355d1521785f879922e5e9cea2ed4be04395d82
Glynn Forrest <me@glynnforrest.com>
2017-05-29 17:44:01 +0100

    tools: docs: Add script and kernel to generate type reference pages

---
+++ bin/gendocs.php
@@ -0,0 +1,24 @@
+#!/usr/bin/env php
+<?php
+
+include __DIR__.'/../vendor/autoload.php';
+
+use Perform\Tools\Documentation\DocGenerator;
+use Perform\Tools\Documentation\DocKernel;
+use Temping\Temping;
+
+$temp = new Temping();
+$kernel = new DocKernel($temp->getDirectory());
+$kernel->boot();
+$c = $kernel->getContainer();
+
+$registry = $c->get('perform_base.type_registry');
+$doc = new DocGenerator($c->get('twig'), $registry);
+
+foreach (array_keys($registry->getAll()) as $name) {
+    $file = sprintf('%s/../docs/reference/types/%s.rst', __DIR__, $name);
+    $doc->generateFile($name, $file);
+    echo 'Generated '.$file.PHP_EOL;
+}
+
+$temp->reset();
