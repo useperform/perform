c8a0a30ee9e94947b6a9aef7719d59cb0f17ff17
Glynn Forrest <me@glynnforrest.com>
2017-06-08 17:18:45 +0100

    dev: YamlModifier can replace top level sections

--- src/DevBundle/File/YamlModifier.php
+++ src/DevBundle/File/YamlModifier.php
@@ -3,6 +3,7 @@
 namespace Perform\DevBundle\File;
 
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * YamlModifier.
@@ -44,5 +45,31 @@ class YamlModifier
         }
 
         return $this->fs->dumpFile($this->configFile, $contents.$yaml);
+    }
+
+    /**
+     * Replace the top level section in the file with new yaml.
+     *
+     * If the section is not found, the yaml will be appended to the file.
+     *
+     * @param string $name
+     * @param string $yaml
+     */
+    public function replaceSection($name, $yaml)
+    {
+        $contents = file_get_contents($this->configFile);
+
+        //m for multiple
+        //s so . matches newlines
+        //match all lines starting from <name>: to the first empty
+        //line (or line made up of spaces).
+        $sectionPattern = sprintf('/^%s:.+^\w*$/ms', $name);
+        $contents = preg_replace($sectionPattern, $yaml, $contents, 1, $count);
+
+        if ($count === 0) {
+            $contents .= PHP_EOL.$yaml;
+        }
+
+        return $this->fs->dumpFile($this->configFile, $contents);
     }
 }
--- src/DevBundle/Tests/File/YamlModifierTest.php
+++ src/DevBundle/Tests/File/YamlModifierTest.php
@@ -5,6 +5,7 @@ namespace Perform\DevBundle\Tests\File;
 use Perform\DevBundle\File\YamlModifier;
 use Perform\DevBundle\BundleResource\ContactBundleResource;
 use Perform\DevBundle\BundleResource\MediaBundleResource;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * YamlModifierTest.
@@ -13,47 +14,93 @@ use Perform\DevBundle\BundleResource\MediaBundleResource;
  **/
 class YamlModifierTest extends \PHPUnit_Framework_TestCase
 {
-    protected $baseFile;
-    protected $testFile;
-    protected $modifiedFile;
-
-    public function setUp()
+    private function routing($file)
     {
-        $this->baseFile = __DIR__.'/../Fixtures/routing.yml';
-        $this->testFile = __DIR__.'/../Fixtures/test_routing.yml';
-        $this->modifiedFile = __DIR__.'/../Fixtures/modified_routing.yml';
-        copy($this->baseFile, $this->testFile);
+        return sprintf('%s/../Fixtures/routing/%s.yml', __DIR__, $file);
+    }
+
+    private function configDev($file)
+    {
+        return sprintf('%s/../Fixtures/config_dev/%s.yml', __DIR__, $file);
     }
 
     public function tearDown()
     {
-        unlink($this->testFile);
+        @unlink($this->routing('actual'));
+        @unlink($this->configDev('actual'));
     }
 
     public function testAddYaml()
     {
-        $this->modifier = new YamlModifier($this->testFile);
-        $this->modifier->addConfig((new MediaBundleResource)->getRoutes());
-        $this->modifier->addConfig((new ContactBundleResource)->getRoutes());
-        $this->assertFileEquals($this->modifiedFile, $this->testFile);
+        copy($this->routing('current'), $this->routing('actual'));
+
+        $mod = new YamlModifier($this->routing('actual'));
+        $mod->addConfig((new MediaBundleResource())->getRoutes());
+        $mod->addConfig((new ContactBundleResource())->getRoutes());
+
+        $this->assertFileEquals($this->routing('expected'), $this->routing('actual'));
     }
 
     public function testBundleIsNotAddedMoreThanOnce()
     {
-        $this->modifier = new YamlModifier($this->testFile);
-        $this->modifier->addConfig((new MediaBundleResource)->getRoutes());
-        $this->modifier->addConfig((new ContactBundleResource)->getRoutes());
+        copy($this->routing('current'), $this->routing('actual'));
 
-        $this->modifier->addConfig((new MediaBundleResource)->getRoutes());
-        $this->modifier->addConfig((new ContactBundleResource)->getRoutes());
-        $this->assertFileEquals($this->modifiedFile, $this->testFile);
+        $mod = new YamlModifier($this->routing('actual'));
+        $mod->addConfig((new MediaBundleResource())->getRoutes());
+        $mod->addConfig((new ContactBundleResource())->getRoutes());
+        $mod->addConfig((new MediaBundleResource())->getRoutes());
+        $mod->addConfig((new ContactBundleResource())->getRoutes());
+
+        $this->assertFileEquals($this->routing('expected'), $this->routing('actual'));
     }
 
     public function testCustomCheckPattern()
     {
-        $this->modifier = new YamlModifier($this->testFile);
+        copy($this->routing('current'), $this->routing('actual'));
 
-        $this->modifier->addConfig((new ContactBundleResource)->getRoutes(), '/^perform_base_dashboard:/m');
-        $this->assertFileEquals($this->baseFile, $this->testFile);
+        $mod = new YamlModifier($this->routing('actual'));
+        $mod->addConfig((new ContactBundleResource())->getRoutes(), '/^perform_base_dashboard:/m');
+
+        $this->assertFileEquals($this->routing('current'), $this->routing('actual'));
+    }
+
+    public function testReplaceSection()
+    {
+        copy($this->configDev('current_with_section'), $this->configDev('actual'));
+
+        $mod = new YamlModifier($this->configDev('actual'));
+        $yaml = Yaml::dump([
+            'perform_dev' => [
+                'some_val' => 'foo',
+                'some_other_val' => ['bar', 'baz'],
+                'skeleton_vars' => [
+                    'app_name' => 'Super app',
+                    'app_description' => 'Awesome',
+                ],
+            ],
+        ], 5);
+        $mod->replaceSection('perform_dev', $yaml);
+
+        $this->assertFileEquals($this->configDev('expected_with_section'), $this->configDev('actual'));
+    }
+
+    public function testReplaceSectionNotExisting()
+    {
+        copy($this->configDev('current_no_section'), $this->configDev('actual'));
+
+        $mod = new YamlModifier($this->configDev('actual'));
+        $yaml = Yaml::dump([
+            'perform_dev' => [
+                'some_val' => 'foo',
+                'some_other_val' => ['bar', 'baz'],
+                'skeleton_vars' => [
+                    'app_name' => 'Super app',
+                    'app_description' => 'Awesome',
+                ],
+            ],
+        ], 5);
+        $mod->replaceSection('perform_dev', $yaml);
+
+        $this->assertFileEquals($this->configDev('expected_no_section'), $this->configDev('actual'));
     }
 }
