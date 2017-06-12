c641421bd02d79ec3208e8ab818ea58768e7b117
Glynn Forrest <me@glynnforrest.com>
2017-06-12 15:59:17 +0100

    base: Install all frontend assets with npm

--- src/BaseBundle/gulpfile.js
+++ src/BaseBundle/gulpfile.js
@@ -1,18 +1,36 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var pump = require('pump');
 
 gulp.task('sass', function (cb) {
-  pump([
-    gulp.src('Resources/scss/app.scss'),
-    sass(),
-    gulp.dest('Resources/public/css'),
-  ], cb);
+  return gulp.src('Resources/scss/app.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('Resources/public/css'));
+});
+
+gulp.task('js', function () {
+  return gulp.src([
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/jquery.cookie/jquery.cookie.js',
+    'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+    'node_modules/underscore/underscore-min.js',
+    'node_modules/backbone/backbone-min.js',
+    'node_modules/moment/min/moment.min.js',
+    'node_modules/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
+    'node_modules/select2/dist/js/select2.min.js',
+  ]).pipe(gulp.dest('Resources/public/js/'));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src([
+    'node_modules/font-awesome/fonts/fontawesome-webfont.*',
+    'node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.*',
+  ]).pipe(gulp.dest('Resources/public/fonts/'));
 });
 
 gulp.task('watch', function () {
   gulp.watch('Resources/scss/*.scss', ['sass']);
+  gulp.watch('Resources/js/*.js', ['js']);
 });
 
-gulp.task('build', ['sass']);
-gulp.task('default', ['sass', 'watch']);
+gulp.task('build', ['sass', 'js', 'fonts']);
+gulp.task('default', ['build', 'watch']);
