3a1923940139fc6f358e3d39a2a4aba2c1e14ace
Glynn Forrest <me@glynnforrest.com>
2017-06-16 18:54:58 +0100

    base: Update data collector test using AdminRegistry

--- src/BaseBundle/Tests/DataCollector/AdminsDataCollectorTest.php
+++ src/BaseBundle/Tests/DataCollector/AdminsDataCollectorTest.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Perform\BaseBundle\Admin\AdminInterface;
 use Perform\BaseBundle\Config\ConfigStoreInterface;
+use Perform\BaseBundle\Doctrine\EntityResolver;
 
 /**
  * AdminsDataCollectorTest.
@@ -20,7 +21,7 @@ class AdminsDataCollectorTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->container = $this->getMock(ContainerInterface::class);
-        $this->registry = new AdminRegistry($this->container);
+        $this->registry = new AdminRegistry($this->container, new EntityResolver());
         $store = $this->getMock(ConfigStoreInterface::class);
         $this->collector = new AdminsDataCollector($this->registry, $store, []);
     }
@@ -31,8 +32,8 @@ class AdminsDataCollectorTest extends \PHPUnit_Framework_TestCase
             ->method('get')
             ->will($this->returnValue($this->getMock(AdminInterface::class)));
 
-        $this->registry->addAdmin('FooBundle:Foo', 'FooBundle\\Entity\\Foo', 'foo_service');
-        $this->registry->addAdmin('FooBundle:Bar', 'FooBundle\\Entity\\Bar', 'bar_service');
+        $this->registry->addAdmin('FooBundle\\Entity\\Foo', 'foo_service');
+        $this->registry->addAdmin('FooBundle\\Entity\\Bar', 'bar_service');
         $this->collector->collect(new Request(), new Response());
         $sortedClasses = ['FooBundle\\Entity\\Bar', 'FooBundle\\Entity\\Foo'];
         $this->assertEquals($sortedClasses, array_keys($this->collector->getAdmins()));
