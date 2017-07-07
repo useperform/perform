1ace55585ab3d745eb6842f4bb6d7e5fb56650cb
Glynn Forrest <me@glynnforrest.com>
2017-07-07 12:48:41 +0100

    rich-content: Start bundle

---
+++ src/RichContentBundle/PerformRichContentBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Perform\RichContentBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class PerformRichContentBundle extends Bundle
+{
+}
