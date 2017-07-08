35503339c526429ec604a9de7add2141be669da0
Glynn Forrest <me@glynnforrest.com>
2017-07-08 17:04:19 +0100

    rich-content: Add block entity

---
+++ src/RichContentBundle/Entity/Block.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace Perform\RichContentBundle\Entity;
+
+/**
+ * A fragment of rich content.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ */
+class Block
+{
+    /**
+     * @var guid
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * @var array
+     */
+    protected $value;
+
+    /**
+     * @var bool
+     */
+    protected $shared = false;
+
+    /**
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * @var \DateTime
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     */
+    protected $updatedAt;
+
+    /**
+     * Set id.
+     *
+     * @param guid $id
+     *
+     * @return Block
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get id.
+     *
+     * @return guid
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type.
+     *
+     * @param string $type
+     *
+     * @return Block
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type.
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set value.
+     *
+     * @param array $value
+     *
+     * @return Block
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value.
+     *
+     * @return array
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set shared.
+     *
+     * @param bool $shared
+     *
+     * @return Block
+     */
+    public function setShared($shared)
+    {
+        $this->shared = $shared;
+
+        return $this;
+    }
+
+    /**
+     * Get shared.
+     *
+     * @return bool
+     */
+    public function getShared()
+    {
+        return $this->shared;
+    }
+
+    /**
+     * Set title.
+     *
+     * @param string $title
+     *
+     * @return Block
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title.
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set createdAt.
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Block
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt.
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt.
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Block
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt.
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Get this block as an array.
+     */
+    public function toArray()
+    {
+        return [
+            'type' => $this->type,
+            'value' => $this->value,
+        ];
+    }
+}
