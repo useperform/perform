6224e5ecbeac0409e3e17a34bbc3be58ce087a33
Glynn Forrest <me@glynnforrest.com>
2017-07-26 10:45:47 +0100

    base: ConfigStore can fetch LabelConfig

--- src/BaseBundle/Config/ConfigStoreInterface.php
+++ src/BaseBundle/Config/ConfigStoreInterface.php
@@ -9,9 +9,43 @@ namespace Perform\BaseBundle\Config;
  **/
 interface ConfigStoreInterface
 {
+    /**
+     * Get the TypeConfig for an entity. The type config may include
+     * overrides from application configuration.
+     *
+     * @param string|object $entity
+     *
+     * @return TypeConfig
+     */
     public function getTypeConfig($entity);
 
+    /**
+     * Get the ActionConfig for an entity. The action config may include
+     * overrides from application configuration.
+     *
+     * @param string|object $entity
+     *
+     * @return ActionConfig
+     */
     public function getActionConfig($entity);
 
+    /**
+     * Get the FilterConfig for an entity. The filter config may include
+     * overrides from application configuration.
+     *
+     * @param string|object $entity
+     *
+     * @return FilterConfig
+     */
     public function getFilterConfig($entity);
+
+    /**
+     * Get the LabelConfig for an entity. The label config may include
+     * overrides from application configuration.
+     *
+     * @param string|object $entity
+     *
+     * @return LabelConfig
+     */
+    public function getLabelConfig($entity);
 }
