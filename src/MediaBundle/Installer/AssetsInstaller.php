218b7ea749c147a8faacedbb722d65f84e7a3d1d
Glynn Forrest <me@glynnforrest.com>
2017-10-06 18:22:30 +0100

    media: Replace install_assets.sh with an installer class

---
+++ src/MediaBundle/Installer/AssetsInstaller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Perform\MediaBundle\Installer;
+
+use Perform\BaseBundle\Installer\InstallerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Perform\BaseBundle\Installer\NpmHelper;
+use Perform\BaseBundle\Installer\ProcessHelper;
+
+/**
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class AssetsInstaller implements InstallerInterface
+{
+    public function install(ContainerInterface $container, LoggerInterface $logger)
+    {
+        $dir = __DIR__.'/..';
+        NpmHelper::install($dir, $logger);
+        ProcessHelper::run('npm run build', $logger, $dir);
+    }
+}
