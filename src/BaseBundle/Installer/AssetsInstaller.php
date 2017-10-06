8a8e0fb9102eb500a02ca6760a656c6bf8aef24e
Glynn Forrest <me@glynnforrest.com>
2017-10-06 18:46:12 +0100

    base: Remove install_assets.sh and simplify installer class
    Remove the logic to search for install_assets.sh scripts.
    
    Assets should be installed with a dedicated installer class now.
    
    This could allow for compatibility with platforms without a bash shell
    in the future.

--- src/BaseBundle/Installer/AssetsInstaller.php
+++ src/BaseBundle/Installer/AssetsInstaller.php
@@ -7,47 +7,14 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Process\Process;
 
 /**
- * AssetsInstaller.
- *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
 class AssetsInstaller implements InstallerInterface
 {
     public function install(ContainerInterface $container, LoggerInterface $logger)
     {
-        $searcher = $container->get('perform_base.bundle_searcher');
-        $files = $searcher->findResourcesAtPath('../install_assets.sh');
-        if (empty($files)) {
-            return;
-        }
-
-        $this->checkRequirements($logger);
-
-        foreach ($files as $file) {
-            $logger->info('Running '.$file);
-
-            $process = new Process(sprintf('(cd %s && %s)', dirname($file), $file));
-            $process->setTimeout(300);
-            $process->mustRun(function ($type, $buffer) use ($logger) {
-                if (Process::ERR === $type) {
-                    $logger->warning($buffer);
-                } else {
-                    $logger->info($buffer);
-                }
-            });
-        }
-    }
-
-    protected function checkRequirements(LoggerInterface $logger)
-    {
-        $process = new Process('which npm');
-        if ($process->run() !== 0) {
-            throw new \RuntimeException('Npm executable not found. Please install nodejs and npm before continuing.');
-        }
-
-        $process = new Process('which yarn');
-        if ($process->run() !== 0) {
-            $logger->warning('Yarn executable not found. Consider installing yarn with "npm install -g yarn" to speed up asset building.'.PHP_EOL);
-        }
+        $dir = __DIR__.'/..';
+        NpmHelper::install($dir, $logger);
+        ProcessHelper::run('npm run build', $logger, $dir);
     }
 }
