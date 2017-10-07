6ac22ed944c224b85abab97e5e4750b6d8401bda
Glynn Forrest <me@glynnforrest.com>
2017-10-07 12:34:34 +0100

    base: Use --no-config to exclude installers that require config

--- src/BaseBundle/Command/InstallCommand.php
+++ src/BaseBundle/Command/InstallCommand.php
@@ -17,6 +17,12 @@ class InstallCommand extends ContainerAwareCommand
             ->setDescription('Install required settings and configuration.')
             ->setHelp('This command should be run <info>after</info> the cache has been warmed up.')
             ->addOption(
+                'no-config',
+                'c',
+                InputOption::VALUE_NONE,
+                "Only run installers that don't require configuration"
+            )
+            ->addOption(
                 'only-installers',
                 'i',
                 InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED,
@@ -28,13 +34,13 @@ class InstallCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        foreach ($this->getInstallers($input) as $installer) {
+        foreach ($this->getInstallers($input, $output) as $installer) {
             $output->writeln(sprintf('Running <info>%s</info>', get_class($installer)));
             $installer->install($this->getContainer(), new ConsoleLogger($output));
         }
     }
 
-    protected function getInstallers(InputInterface $input)
+    protected function getInstallers(InputInterface $input, OutputInterface $output)
     {
         $classes = $this->getContainer()->get('perform_base.bundle_searcher')
                  ->findClassesWithNamespaceSegment(
@@ -51,22 +57,46 @@ class InstallCommand extends ContainerAwareCommand
             $installers[] = $r->newInstance();
         }
 
-        $only = $input->getOption('only-installers');
-        if (!empty($only)) {
-            $installers = array_filter($installers, function ($installer) use ($only) {
-                $class = get_class($installer);
-                $pieces = explode('\\', $class);
-                $end = strtolower(end($pieces));
-                foreach ($only as $item) {
-                    if ($item === $class || strtolower($item) === $end || strtolower($item).'installer' === $end) {
-                        return true;
-                    }
-                }
+        $installers = $this->filterNames($installers, $input->getOption('only-installers'));
 
-                return false;
-            });
+        return $this->filterNoConfig($output, $installers, $input->getOption('no-config'));
+    }
+
+    protected function filterNames(array $installers, array $only)
+    {
+        if (empty($only)) {
+            return $installers;
         }
 
-        return $installers;
+        return array_filter($installers, function ($installer) use ($only) {
+            $class = get_class($installer);
+            $pieces = explode('\\', $class);
+            $end = strtolower(end($pieces));
+            foreach ($only as $item) {
+                if ($item === $class || strtolower($item) === $end || strtolower($item).'installer' === $end) {
+                    return true;
+                }
+            }
+
+            return false;
+        });
+    }
+
+    protected function filterNoConfig(OutputInterface $output, array $installers, $noConfig)
+    {
+        if (!$noConfig) {
+            return $installers;
+        }
+
+        return array_filter($installers, function ($installer) use ($output) {
+            if (!$installer->requiresConfiguration()) {
+                return true;
+            }
+            if ($output->isVerbose()) {
+                $output->writeln(sprintf('Skipping <info>%s</info> as it requires configuration', get_class($installer)));
+            }
+
+            return false;
+        });
     }
 }
