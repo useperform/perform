b7d631ab38dcbeff66ec082ec288cbd27657ef8d
Glynn Forrest <me@glynnforrest.com>
2017-11-13 08:56:29 +0000

    base: Each task has an id so it can be modified later

--- src/BaseBundle/Resources/js/tasks.js
+++ src/BaseBundle/Resources/js/tasks.js
@@ -1,30 +1,55 @@
-var Tasks = {
-  props: ['tasks'],
-  data: function () {
-    return {
-      tasks: [],
-    };
-  },
-  template: '#template-tasks'
-};
+(function () {
+  var Tasks = {
+    props: ['tasks'],
+    data: function () {
+      return {
+        tasks: [],
+      };
+    },
+    template: '#template-tasks'
+  };
 
-var taskapp = new Vue({
-  el: '#perform-tasks',
-  data: {
-    tasks: []
-  },
-  components: {
-    'p-tasks': Tasks,
-  }
-});
-
-window.Perform = window.Perform ? window.Perform : {};
-window.Perform.func = window.Perform.func ? window.Perform.func : {};
-
-window.Perform.func.addTask = function(title, current, progress) {
-  taskapp.tasks.push({
-    title: title,
-    current: current,
-    progress: progress,
+  var taskapp = new Vue({
+    el: '#perform-tasks',
+    data: {
+      tasks: []
+    },
+    components: {
+      'p-tasks': Tasks,
+    }
   });
-};
+
+  var addTask = function(title, current, max) {
+    if (!current) {
+      current = 0;
+    }
+    if (!max) {
+      max = 100;
+    }
+    taskapp.tasks.push({
+      title: title,
+      current: current,
+      max: max,
+    });
+
+    return taskapp.tasks.length - 1;
+  };
+
+  var getTask = function(index) {
+    return taskapp.tasks[index];
+  };
+
+  var setTaskProgress = function(index, current) {
+    getTask(index).current = current;
+  };
+
+  if (!window.Perform) {
+    window.Perform = {};
+  }
+  if (!window.Perform.base) {
+    window.Perform.base = {};
+  }
+  window.Perform.base.addTask = addTask;
+  window.Perform.base.getTask = getTask;
+  window.Perform.base.setTaskProgress = setTaskProgress;
+})();
