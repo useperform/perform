08288c5312c74a5383cd6c2ec06dfda197389bf7
Glynn Forrest <me@glynnforrest.com>
2017-11-16 10:55:15 +0000

    media: Add bootstrap-vue for use in media selector

--- src/MediaBundle/Resources/src/components/MediaTypeEditor.vue
+++ src/MediaBundle/Resources/src/components/MediaTypeEditor.vue
@@ -1,13 +1,30 @@
 <template>
-  <a class="btn btn-primary">{{msg}}</a>
+  <div>
+    <b-btn v-b-modal.modal1>Select files</b-btn>
+
+    <b-modal id="modal1" title="Select files" visible>
+      <p>{{msg}}</p>
+    </b-modal>
+  </div>
 </template>
 
 <script>
+import bButton from 'bootstrap-vue/es/components/button/button'
+import bModal from 'bootstrap-vue/es/components/modal/modal'
+import bModalDirective from 'bootstrap-vue/es/directives/modal/modal'
+
 export default {
   data () {
     return {
       msg: 'Select files'
     }
-  }
+  },
+  components: {
+    'b-btn': bButton,
+    'b-modal': bModal
+  },
+  directives: {
+    'b-modal' : bModalDirective
+  },
 }
 </script>
