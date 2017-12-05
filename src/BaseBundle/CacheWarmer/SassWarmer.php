3e7a9627b852611dcdadced326832d42f184aa9c
Glynn Forrest <me@glynnforrest.com>
2017-12-05 11:32:11 +0000

    base: Add extra_sass files to the sass build

--- src/BaseBundle/CacheWarmer/SassWarmer.php
+++ src/BaseBundle/CacheWarmer/SassWarmer.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
 use Perform\BaseBundle\Util\BundleSearcher;
 use Perform\BaseBundle\Asset\ThemeNotFoundException;
 use Symfony\Component\Filesystem\Filesystem;
+use Perform\BaseBundle\Asset\AssetNotFoundException;
 
 /**
  * Find additional files to be included in scss compilation, such as
@@ -16,23 +17,29 @@ use Symfony\Component\Filesystem\Filesystem;
 class SassWarmer implements CacheWarmerInterface
 {
     protected $bundleSearcher;
-    protected $theme;
     protected $fs;
+    protected $theme;
+    protected $extraFiles = [];
 
     const TARGET_DIR = __DIR__.'/../Resources/scss/';
 
-    public function __construct(BundleSearcher $bundleSearcher, Filesystem $fs, $theme)
+    public function __construct(BundleSearcher $bundleSearcher, Filesystem $fs, $theme, array $extraFiles)
     {
         $this->bundleSearcher = $bundleSearcher;
         $this->theme = $theme;
         $this->fs = $fs;
+        $this->extraFiles = $extraFiles;
     }
 
     public function warmUp($cacheDir)
     {
-        $pieces = explode(':', $this->theme);
-        $themeBundle = $pieces[0];
-        $themeName = $pieces[1];
+        $this->loadTheme();
+        $this->loadExtraSass();
+    }
+
+    public function loadTheme()
+    {
+        list($themeBundle, $themeName) = $this->parseAsset($this->theme);
         $themeFiles = [
             'theme.scss' => '_theme.scss',
             'variables.scss' => '_theme_variables.scss',
@@ -50,10 +57,50 @@ class SassWarmer implements CacheWarmerInterface
                 throw ThemeNotFoundException::missingFile($this->theme, $source);
             }
 
-            $importPath = rtrim($this->fs->makePathRelative($files[0]->getPathname(), self::TARGET_DIR), '/');
-            $content = sprintf('@import "%s";', $importPath);
-            $this->fs->dumpFile(self::TARGET_DIR.$target, $content);
+            $this->dumpSassImport($target, [$files[0]->getPathname()]);
         }
+    }
+
+    public function loadExtraSass()
+    {
+        $paths = [];
+        foreach ($this->extraFiles as $asset) {
+            list($bundle, $name) = $this->parseAsset($asset);
+            try {
+                $relativePath = sprintf('scss/%s', $name);
+
+                $files = array_values(iterator_to_array($this->bundleSearcher->findResourcesAtPath($relativePath, [$bundle])));
+            } catch (\Exception $e) {
+                throw AssetNotFoundException::missing($asset);
+            }
+            if (count($files) < 1) {
+                throw AssetNotFoundException::missing($asset);
+            }
+            $paths[] = $files[0]->getPathname();
+        }
+
+        $this->dumpSassImport('_extras.scss', $paths);
+    }
+
+    private function parseAsset($name)
+    {
+        $pieces = explode(':', $name);
+        if (count($pieces) !== 2) {
+            throw AssetNotFoundException::invalid($name);
+        }
+
+        return $pieces;
+    }
+
+    private function dumpSassImport($target, array $paths)
+    {
+        $content = '';
+        foreach ($paths as $path) {
+            $importPath = rtrim($this->fs->makePathRelative($path, self::TARGET_DIR), '/');
+            $content = sprintf('@import "%s";'.PHP_EOL, $importPath);
+        }
+
+        $this->fs->dumpFile(self::TARGET_DIR.$target, $content);
     }
 
     public function isOptional()
