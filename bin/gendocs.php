87d0746dc4e0930993dbf610b310fe8740880dde
Glynn Forrest <me@glynnforrest.com>
2017-12-06 13:34:35 +0000

    docs: Add generated sass variable reference

--- bin/gendocs.php
+++ bin/gendocs.php
@@ -5,6 +5,7 @@ include __DIR__.'/../vendor/autoload.php';
 
 use Perform\Tools\Documentation\DocGenerator;
 use Perform\Tools\Documentation\DocKernel;
+use Perform\Tools\Documentation\SassReferenceGenerator;
 use Temping\Temping;
 
 $temp = new Temping();
@@ -25,5 +26,11 @@ foreach (array_keys($registry->getAll()) as $name) {
     $doc->generateFile($name, $file);
     echo 'Generated '.$file.PHP_EOL;
 }
+
+$source = __DIR__.'/../src/BaseBundle/Resources/scss/variables.scss';
+$target = __DIR__.'/../docs/reference/sass.rst';
+$sass = new SassReferenceGenerator($twig);
+$sass->generateFile($source, $target);
+echo 'Generated '.$target.PHP_EOL;
 
 $temp->reset();
