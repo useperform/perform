f20b2d1b66a59b9d0ca30a4bde9463ee7d5e47b6
Glynn Forrest <me@glynnforrest.com>
2017-12-21 21:33:17 +0000

    mailing-list: Define configuration to register multiple connectors

---
+++ src/MailingListBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Perform\MailingListBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * @author Glynn Forrest <me@glynnforrest.com>
+ */
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('perform_mailing_list');
+
+        $rootNode
+            ->fixXmlConfig('connector')
+            ->children()
+                ->arrayNode('connectors')
+                    ->useAttributeAsKey('name')
+                    ->prototype('array')
+                    ->children()
+                        ->enumNode('connector')
+                            ->values(['local'])
+                            ->isRequired()
+                        ->end()
+                        ->scalarNode('entity_manager')->defaultNull()->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
--- src/MailingListBundle/DependencyInjection/PerformMailingListExtension.php
+++ src/MailingListBundle/DependencyInjection/PerformMailingListExtension.php
@@ -8,9 +8,12 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 use Perform\MailingListBundle\Form\Type\EmailOnlyType;
 use Perform\MailingListBundle\Form\Type\EmailAndNameType;
+use Perform\MailingListBundle\Connector\LocalConnector;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
- * PerformMailingListExtension.
+ * @author Glynn Forrest <me@glynnforrest.com>
  **/
 class PerformMailingListExtension extends Extension
 {
@@ -19,11 +22,35 @@ class PerformMailingListExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = $this->getConfiguration($configs, $container);
+        $config = $this->processConfiguration($configuration, $configs);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
         $factory = $container->getDefinition('perform_mailing_list.form_factory');
         $factory->addMethodCall('addType', ['email_only', EmailOnlyType::class]);
         $factory->addMethodCall('addType', ['email_name', EmailAndNameType::class]);
+
+        $connectors = [];
+        foreach ($config['connectors'] as $name => $connectorConfig) {
+            $connectors[$name] = $this->createConnector($container, $connectorConfig);
+        }
+
+        $container->getDefinition('perform_mailing_list.manager')
+            ->setArgument(1, $connectors);
+    }
+
+    protected function createConnector(ContainerBuilder $container, array $config)
+    {
+        switch ($config['connector']) {
+        case 'local':
+            $def = new Definition(LocalConnector::class);
+            $def->setArgument(0, new Reference(sprintf('doctrine.orm.%s_entity_manager', $config['entity_manager'] ?: 'default')));
+
+            return $def;
+        }
+
+        throw new \Exception(sprintf('Unable to configure unknown mailing list connector type "%s"', $config['connector']));
     }
 }
