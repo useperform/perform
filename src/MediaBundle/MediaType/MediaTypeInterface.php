0869a90a30b02433a012824c745a58f629862f6f
Glynn Forrest <me@glynnforrest.com>
2018-02-01 12:28:41 +0000

    media: Add getSuitableLocation method for media types

--- src/MediaBundle/MediaType/MediaTypeInterface.php
+++ src/MediaBundle/MediaType/MediaTypeInterface.php
@@ -17,4 +17,15 @@ interface MediaTypeInterface
     public function supports(File $file, MediaResource $resource);
 
     public function process(File $file, MediaResource $pathname, BucketInterface $bucket);
+
+    /**
+     * Get a Location object for a file that matches the given criteria.
+     *
+     * For example, if the criteria specifies files that are a
+     * particular size, the media type should return a file closest to
+     * that size.
+     *
+     * @return Location
+     */
+    public function getSuitableLocation(File $file, array $criteria);
 }
