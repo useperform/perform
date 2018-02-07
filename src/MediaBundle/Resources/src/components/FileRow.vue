3b84c23316fa181da6ebc3f0c6fc68f9211cec7c
Glynn Forrest <me@glynnforrest.com>
2018-02-07 18:11:22 +0000

    media: Add view and download links in the file listing

--- src/MediaBundle/Resources/src/components/FileRow.vue
+++ src/MediaBundle/Resources/src/components/FileRow.vue
@@ -10,6 +10,12 @@
       {{file.humanType}}
     </div>
     <div class="col-4 actions">
+      <a :href="file.url" target="_blank">
+        <i class="fa fa-lg fa-eye"></i>
+      </a>
+      <a :href="file.url" :download="file.name">
+        <i class="fa fa-lg fa-download"></i>
+      </a>
       <a href="#" @click.prevent="remove" :disabled="removing">
         <i class="fa fa-spin fa-circle-o-notch" v-if="removing"></i>
         <i class="fa fa-lg fa-trash" v-else></i>
