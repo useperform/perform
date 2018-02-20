8d176e1ed4a8efd4529caa5c98722303f62bb322
Glynn Forrest <me@glynnforrest.com>
2018-02-20 00:37:29 +0000

    media: Begin work on youtube media type

---
+++ src/MediaBundle/MediaType/YoutubeType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Perform\MediaBundle\MediaType;
+
+use Perform\MediaBundle\Entity\File;
+use Perform\MediaBundle\MediaResource;
+use Perform\MediaBundle\Bucket\BucketInterface;
+use Perform\MediaBundle\Event\ImportUrlEvent;
+
+/**
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class YoutubeType implements MediaTypeInterface
+{
+    public static function getName()
+    {
+        return 'youtube';
+    }
+
+    public function supports(File $file, MediaResource $resource)
+    {
+        if ($resource->isFile() || !preg_match("`^youtube:(\w+)$`", $resource->getPath(), $matches)) {
+            return false;
+        }
+        $resource->setPath($matches[1]);
+
+        return true;
+    }
+
+    public function onUrlImport(ImportUrlEvent $event)
+    {
+        $urlRegex = "`(https?://)?(www.)?(youtube.com/watch\?v=|youtu.be/)(?P<id>\w+)`";
+        // check for false positives, like /favicon
+        if (!preg_match($urlRegex, $event->getUrl(), $matches)) {
+            return;
+        }
+        $youtubeId = $matches['id'];
+        // fetch the title from the page
+        $name = 'Youtube video '.$youtubeId;
+        $resource = new MediaResource('youtube:'.$youtubeId, $name, $event->getOwner());
+
+        $event->addResource($resource);
+    }
+
+    public function process(File $file, MediaResource $resource, BucketInterface $bucket)
+    {
+    }
+
+    public function getSuitableLocation(File $file, array $criteria)
+    {
+        if (isset($criteria['width']) && count($file->getExtraLocations()) > 0) {
+            return $file->getExtraLocations()[0];
+        }
+
+        return $file->getPrimaryLocation();
+    }
+}
---
+++ src/MediaBundle/Tests/MediaType/YoutubeTypeTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Perform\MediaBundle\Tests\MediaType;
+
+use Perform\MediaBundle\MediaType\YoutubeType;
+use Perform\MediaBundle\Entity\File;
+use Perform\MediaBundle\MediaResource;
+use Perform\MediaBundle\Event\ImportUrlEvent;
+
+/**
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class YoutubeTypeTest extends \PHPUnit_Framework_TestCase
+{
+    protected $type;
+
+    public function setUp()
+    {
+        $this->type = new YoutubeType();
+    }
+
+    public function urlProvider()
+    {
+        return [
+            ['https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'dQw4w9WgXcQ'],
+            ['http://www.youtube.com/watch?v=dQw4w9WgXcQ', 'dQw4w9WgXcQ'],
+            ['youtube.com/watch?v=dQw4w9WgXcQ', 'dQw4w9WgXcQ'],
+            ['https://youtu.be/dQw4w9WgXcQ', 'dQw4w9WgXcQ'],
+            ['youtu.be/dQw4w9WgXcQ', 'dQw4w9WgXcQ'],
+        ];
+    }
+
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testUrlImportSupportsDifferentRegexSchemes($url, $id)
+    {
+        $event = new ImportUrlEvent($url);
+        $this->type->onUrlImport($event);
+
+        $this->assertSame(1, count($event->getResources()));
+        $this->assertSame('youtube:'.$id, $event->getResources()[0]->getPath());
+    }
+
+    public function testSupports()
+    {
+        $file = new File();
+        $id = 'dQw4w9WgXcQ';
+        $resource = new MediaResource('youtube:'.$id);
+
+        $this->assertTrue($this->type->supports($file, $resource));
+        $this->assertSame($id, $resource->getPath());
+    }
+}
