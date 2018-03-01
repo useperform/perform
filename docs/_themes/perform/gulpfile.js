0d7c927c39d24d216ca3b584e8938d3a5118d890
Glynn Forrest <me@glynnforrest.com>
2018-03-01 23:21:48 +0000

    docs: Replace sphinx-autobuild with a gulp browsersync server

--- docs/_themes/perform/gulpfile.js
+++ docs/_themes/perform/gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var run = require('gulp-run');
+var browserSync = require('browser-sync').create();
 
 function handleError (err) {
   console.log(err);
@@ -15,13 +17,35 @@ gulp.task('sass', function () {
   return gulp.src('scss/theme.scss')
     .pipe(sass())
     .on('error', handleError)
-    .pipe(gulp.dest('static/'));
+    .pipe(gulp.dest('static/'))
+  // need to pipe to the build output directory so browsersync works
+    .pipe(gulp.dest('../../_build/html/_static/'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
   gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('../../**/*.rst', ['html']);
+  gulp.watch('*.html', ['html']);
 });
 
-gulp.task('dev', ['watch', 'sass']);
-gulp.task('build', ['sass', 'js']);
+gulp.task('html', function () {
+  run('make html', {
+    cwd: '../../'
+  })
+    .exec()
+    .on('error', handleError);
+});
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    open: false,
+    server: {
+      baseDir: "../../_build/html"
+    }
+  });
+});
+
+gulp.task('dev', ['watch', 'js', 'sass', 'browser-sync']);
+gulp.task('build', ['sass', 'js', 'html']);
 gulp.task('default', ['dev']);
