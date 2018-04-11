30b68b5980393983bd7cb4ae6241c2be33783c57
Glynn Forrest <me@glynnforrest.com>
2018-04-11 00:00:12 +0000

    page-editor: Select available page versions from the toolbar

--- src/PageEditorBundle/Resources/js/components/Toolbar.vue
+++ src/PageEditorBundle/Resources/js/components/Toolbar.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="p--local">
     <div class="p-comp-page-editor-toolbar">
-      <a class="btn btn-primary" @click.prevent="save">
+      <a class="version-menu-toggler" @click.prevent="toggleVersionMenu">
+        {{currentTitle}}
+      </a>
+      <ul v-if="showingMenu" class="version-menu">
+        <li @click.prevent="loadVersion(version.id)" v-for="version in versions">
+          {{version.title}}
+        </li>
+      </ul>
+      <a @click.prevent="save">
         Save
       </a>
-      <a class="btn btn-light" :href="finishUrl">
+      <a @click.prevent="publish">
+        Publish
+      </a>
+      <a :href="finishUrl">
         Finish editing
       </a>
     </div>
@@ -17,9 +28,38 @@
      'finishUrl',
    ],
 
+   data() {
+     return {
+       showingMenu: false,
+     };
+   },
+
+   computed: {
+     currentTitle() {
+       return this.$store.state.versionTitle;
+     },
+     versions() {
+       return this.$store.state.versions;
+     }
+   },
+
    methods: {
      save() {
        this.$store.dispatch('save');
+     },
+
+     publish() {
+     },
+
+     loadVersion(versionId) {
+       this.$store.dispatch('loadVersion', {
+         versionId
+       });
+       this.showingMenu = false;
+     },
+
+     toggleVersionMenu() {
+       this.showingMenu = !this.showingMenu;
      }
    }
  };
