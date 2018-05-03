ba44f6c46659f7531cef39d6740f2dd27a71fdae
Glynn Forrest <me@glynnforrest.com>
2018-05-03 23:18:48 +0000

    dev: Update create theme command to use new syntax

--- src/DevBundle/Command/CreateThemeCommand.php
+++ src/DevBundle/Command/CreateThemeCommand.php
@@ -7,56 +7,35 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Perform\DevBundle\File\FileCreator;
-use Symfony\Component\Console\Question\Question;
 
 /**
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
 class CreateThemeCommand extends ContainerAwareCommand
 {
+    protected $creator;
+
+    public function __construct(FileCreator $creator)
+    {
+        $this->creator = $creator;
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this->setName('perform-dev:create:theme')
-            ->setDescription('Create scss theme files')
-            ->addArgument('theme', InputArgument::OPTIONAL, 'The namespaced theme name, e.g. AppBundle:my_theme');
+            ->setDescription('Create scss theme files in a given directory')
+            ->addArgument('directory', InputArgument::REQUIRED, 'The directory to store the theme files, e.g. src/Resources/scss/themes/my-theme');
 
         FileCreator::addInputOptions($this);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        list($bundle, $theme) = $this->getTheme($input, $output);
+        $dir = trim($input->getArgument('directory'), '/').'/';
+        $this->creator->create($dir.'variables.scss', $this->creator->render('theme_variables.scss.twig'));
+        $this->creator->create($dir.'theme.scss', $this->creator->render('theme.scss.twig'));
 
-        $creator = $this->getContainer()->get('perform_dev.file_creator');
-        $vars = ['theme' => $theme];
-        $creator->createInBundle($bundle, sprintf('Resources/scss/themes/%s/variables.scss', $theme), 'theme_variables.scss.twig', $vars);
-        $creator->createInBundle($bundle, sprintf('Resources/scss/themes/%s/theme.scss', $theme), 'theme.scss.twig', $vars);
-
-        $output->writeln(['', sprintf('To use this theme, set the <comment>perform_base.theme</comment> configuration option to <info>%s:%s</info>.', $bundle->getName(), $theme)]);
-    }
-
-    protected function getTheme(InputInterface $input, OutputInterface $output)
-    {
-        $kernel = $this->getContainer()->get('kernel');
-
-        if ($input->getArgument('theme')) {
-            $full = $input->getArgument('theme');
-            $pieces = explode(':', $full, 2);
-            if (count($pieces) !== 2) {
-                throw new \RuntimeException('Theme name must be of the form <Bundle>:<theme>, e.g. AppBundle:my_theme');
-            }
-            $bundle = $kernel->getBundle($pieces[0]);
-
-            return [$bundle, $pieces[1]];
-        }
-
-        $q = new Question('Bundle to create the theme in: ');
-        $q->setAutocompleterValues(array_keys($kernel->getBundles()));
-        $bundle = $kernel->getBundle($this->getHelper('question')->ask($input, $output, $q));
-
-        $q = new Question('Name of the theme, e.g. <info>my_theme</info>: ');
-        $theme = $this->getHelper('question')->ask($input, $output, $q);
-
-        return [$bundle, $theme];
+        $output->writeln(['', 'To use this theme, ensure an asset namespace exists for a parent directory (e.g. app -> src/Resources), then set the <comment>perform_base.assets.theme</comment> configuration node to the theme directory (e.g. "~app/scss/themes/my-theme).', 'Remember to start the reference with a tilde (~).']);
     }
 }
