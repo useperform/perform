1edc1ec8c03ae96b003487a47218f73986c7efc8
Glynn Forrest <me@glynnforrest.com>
2018-05-08 14:42:05 +0000

    base: Fix test for assets class

--- src/BaseBundle/Tests/DependencyInjection/AssetsTest.php
+++ src/BaseBundle/Tests/DependencyInjection/AssetsTest.php
@@ -10,13 +10,30 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
  **/
 class AssetsTest extends \PHPUnit_Framework_TestCase
 {
-    public function testExtraAssetsCanBeAdded()
+    public function testExtraJs()
     {
         $container = new ContainerBuilder();
-        Assets::addExtraSass($container, ['one.scss']);
+        Assets::addExtraJavascript($container, 'one', 'one.js');
+        $this->assertSame(['one' => 'one.js'], $container->getParameter(Assets::PARAM_EXTRA_JS));
+
+        Assets::addExtraJavascript($container, 'two', 'two.js');
+        Assets::addExtraJavascript($container, 'three', 'three.js');
+        $expected = [
+            'one' => 'one.js',
+            'two' => 'two.js',
+            'three' => 'three.js'
+        ];
+        $this->assertSame($expected, $container->getParameter(Assets::PARAM_EXTRA_JS));
+    }
+
+    public function testExtraSass()
+    {
+        $container = new ContainerBuilder();
+        Assets::addExtraSass($container, 'one.scss');
         $this->assertSame(['one.scss'], $container->getParameter(Assets::PARAM_EXTRA_SASS));
 
-        Assets::addExtraSass($container, ['two.scss', 'three.scss']);
+        Assets::addExtraSass($container, 'two.scss');
+        Assets::addExtraSass($container, 'three.scss');
         $this->assertSame(['one.scss', 'two.scss', 'three.scss'], $container->getParameter(Assets::PARAM_EXTRA_SASS));
     }
 }
