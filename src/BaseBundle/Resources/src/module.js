712a9624af3cd86cff28a393f08c77c8e273c7e5
Glynn Forrest <me@glynnforrest.com>
2018-05-10 23:38:34 +0000

    base: Fix undefined function error for js message helpers

--- src/BaseBundle/Resources/src/module.js
+++ src/BaseBundle/Resources/src/module.js
@@ -3,34 +3,36 @@ import {datepicker, markdown} from './form';
 
 let messages = [];
 
+const showMessage = function(type, message) {
+  var template = _.template($('#template-flash-message').html());
+  var id = Date.now();
+  var msg = {
+    id: id,
+    type: type,
+    message: message
+  };
+  //store message history for possible fanciness later
+  messages.push(msg);
+  $('.admin-flash-messages').html(template(msg));
+
+  setTimeout(function() {
+    $('#flash-message-'+id).slideUp();
+  }, 3000);
+}
+
 export default {
   fancyForm(form) {
     form.find('.select2').select2();
   },
 
-  showMessage(type, message) {
-    var template = _.template($('#template-flash-message').html());
-    var id = Date.now();
-    var msg = {
-      id: id,
-      type: type,
-      message: message
-    };
-    //store message history for possible fanciness later
-    messages.push(msg);
-    $('.admin-flash-messages').html(template(msg));
-
-    setTimeout(function() {
-      $('#flash-message-'+id).slideUp();
-    }, 3000);
-  },
+  showMessage,
 
   showError(message) {
-    return this.showMessage('danger', message);
+    return showMessage('danger', message);
   },
 
   showSuccess(message) {
-    return this.showMessage('success', message);
+    return showMessage('success', message);
   },
 
   tasks: {
