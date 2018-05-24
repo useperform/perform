4921918d29e074638dd8f9e0a48c103e2b6ddfa4
Glynn Forrest <me@glynnforrest.com>
2018-05-24 10:20:07 +0000

    base: CrudVoter supports both crud names and entity objects

--- src/BaseBundle/Security/CrudVoter.php
+++ src/BaseBundle/Security/CrudVoter.php
@@ -6,9 +6,18 @@ use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Perform\BaseBundle\Crud\CrudRegistry;
 use Perform\BaseBundle\Routing\CrudUrlGeneratorInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
- * Grant permission to execute actions to entities by default.
+ * Grant permission to all crud contexts by default.
+ *
+ * It allows access for both the crud name
+ * is_granted('VIEW', 'some_crud')
+ * as well as a specific entity object
+ * is_granted('VIEW', $entity)
+ *
+ * This voter is designed to be used with the 'unanimous' security strategy.
+ * Add other voters to restrict access to certain contexts and entities.
  *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
@@ -16,29 +25,52 @@ class CrudVoter extends Voter
 {
     protected $crudRegistry;
     protected $urlGenerator;
+    protected $em;
+    // results of doctrine metadata and crud checks cached for speed
+    protected $classSupportsCache = [];
 
-    public function __construct(CrudRegistry $crudRegistry, CrudUrlGeneratorInterface $urlGenerator)
+    public function __construct(CrudRegistry $crudRegistry, CrudUrlGeneratorInterface $urlGenerator, EntityManagerInterface $em)
     {
         $this->crudRegistry = $crudRegistry;
         $this->urlGenerator = $urlGenerator;
+        $this->em = $em;
     }
 
     public function supports($attribute, $subject)
     {
-        return $this->crudRegistry->has($subject);
+        if (is_string($subject) && $this->crudRegistry->has($subject)) {
+            return true;
+        }
+        if (!is_object($subject)) {
+            return false;
+        }
+
+        $class = get_class($subject);
+        if (!isset($this->classSupportsCache[$class])) {
+            $this->classSupportsCache[$class] =
+                                              // is a doctrine entity
+                                              !$this->em->getMetadataFactory()->isTransient($class)
+                                              // has a crud
+                                              && $this->crudRegistry->hasForEntity($class);
+        }
+
+        return $this->classSupportsCache[$class];
     }
 
     public function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
-        switch ($attribute) {
-        case 'VIEW':
-            return $this->urlGenerator->routeExists($subject, 'view');
-        case 'EDIT':
-            return $this->urlGenerator->routeExists($subject, 'edit');
-        case 'DELETE':
+        return is_string($subject)
+            ? $this->voteOnCrudName($attribute, $subject)
+            : in_array($attribute, ['VIEW', 'EDIT', 'DELETE']);
+    }
+
+    private function voteOnCrudName($attribute, $crudName)
+    {
+        if ($attribute === 'DELETE') {
             return true;
-        default:
-            return false;
         }
+
+        return in_array($attribute, ['VIEW', 'EDIT']) &&
+            $this->urlGenerator->routeExists($crudName, strtolower($attribute));
     }
 }
