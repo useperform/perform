3be42683bfac766ae8a530890208b1d1c279df78
Glynn Forrest <me@glynnforrest.com>
2018-05-24 10:56:03 +0000

    base: Update action run command to use crud names
    Authenticate as a basic user so the actions are granted.

--- src/BaseBundle/Command/ActionRunCommand.php
+++ src/BaseBundle/Command/ActionRunCommand.php
@@ -6,10 +6,9 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken;
 
 /**
- * ActionRunCommand.
- *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
 class ActionRunCommand extends ContainerAwareCommand
@@ -18,16 +17,20 @@ class ActionRunCommand extends ContainerAwareCommand
     {
         $this->setName('perform:action:run')
             ->setDescription('Run an action on an entity or list of entities')
+            ->addArgument('crudName', InputArgument::REQUIRED, 'The crud name')
             ->addArgument('action', InputArgument::REQUIRED)
-            ->addArgument('entityClass', InputArgument::REQUIRED, 'The entity name or class, e.g. PerformUserBundle:User')
             ->addArgument('entity', InputArgument::IS_ARRAY, 'An entity id or list of entity ids')
             ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        // authenticate as a basic user so the action runner will grant permission to run actions
+        $this->getContainer()->get('security.token_storage')
+            ->setToken(new PreAuthenticatedToken('console_user', null, 'console', ['ROLE_USER']));
+
         $response = $this->getContainer()->get('perform_base.action_runner')
-                  ->run($input->getArgument('action'), $input->getArgument('entityClass'), $input->getArgument('entity'));
+                  ->run($input->getArgument('crudName'), $input->getArgument('action'), $input->getArgument('entity'));
 
         $output->writeln($response->getMessage());
     }
