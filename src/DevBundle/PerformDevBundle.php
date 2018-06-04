c18166a192d6a904bda3d204d1abf74624d06078
Glynn Forrest <me@glynnforrest.com>
2018-06-04 22:48:51 +0000

    dev: Remove the concept of frontends, they are too specialized

--- src/DevBundle/PerformDevBundle.php
+++ src/DevBundle/PerformDevBundle.php
@@ -3,19 +3,10 @@
 namespace Perform\DevBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Perform\DevBundle\DependencyInjection\Compiler\AddFrontendsPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
- * PerformDevBundle.
- *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
 class PerformDevBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-        $container->addCompilerPass(new AddFrontendsPass());
-    }
 }
