2839a198a1be6ff8a1dcbe025a3ff4567f04bf2a
Glynn Forrest <me@glynnforrest.com>
2018-06-08 15:02:42 +0000

    base: Menu items can be sorted by priority
    Set the extra 'priority' value to an integer for each menu item.

--- src/BaseBundle/Menu/MenuBuilder.php
+++ src/BaseBundle/Menu/MenuBuilder.php
@@ -5,6 +5,7 @@ namespace Perform\BaseBundle\Menu;
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Perform\BaseBundle\Event\MenuEvent;
+use Knp\Menu\ItemInterface;
 
 /**
  * @author Glynn Forrest <me@glynnforrest.com>
@@ -26,6 +27,24 @@ class MenuBuilder
 
         $this->dispatcher->dispatch(MenuEvent::BUILD, new MenuEvent('perform_sidebar', $menu, $this->factory));
 
+        $this->sortByPriority($menu);
+
         return $menu;
+    }
+
+    public function sortByPriority(ItemInterface $menu)
+    {
+        $order = [];
+        foreach ($menu as $name => $item) {
+            $order[$name] = $item->getExtra('priority', 0);
+
+            if ($item->hasChildren()) {
+                $this->sortByPriority($item);
+            }
+        }
+
+        arsort($order);
+
+        $menu->reorderChildren(array_keys($order));
     }
 }
---
+++ src/BaseBundle/Tests/Menu/MenuBuilderTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Perform\BaseBundle\Tests\Menu;
+
+use Knp\Menu\MenuItem;
+use Perform\BaseBundle\Menu\MenuBuilder;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\MenuFactory;
+
+/**
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class MenuBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    protected $factory;
+    protected $dispatcher;
+    protected $builder;
+
+    public function setUp()
+    {
+        $this->factory = new MenuFactory();
+        $this->dispatcher = $this->getMock(EventDispatcherInterface::class);
+        $this->builder = new MenuBuilder($this->factory, $this->dispatcher);
+    }
+
+    public function testSortByPriority()
+    {
+        $menu = new MenuItem('base', $this->factory);
+        $menu->addChild('one');
+        $menu->addChild('two');
+        $menu->addChild('three')->setExtra('priority', 40);
+
+        $this->builder->sortByPriority($menu);
+        $this->assertSame(['three', 'two', 'one'], array_keys($menu->getChildren()));
+    }
+
+    public function testSortByPriorityRecursively()
+    {
+        $menu = new MenuItem('', $this->factory);
+        $menu->addChild('one');
+        $two = $menu->addChild('two');
+        $two->addChild('two.one')->setExtra('priority', 60);
+        $two->addChild('two.two')->setExtra('priority', 100);
+        $three = $menu->addChild('three');
+        $three->setExtra('priority', 40);
+
+        $this->builder->sortByPriority($menu);
+        $this->assertSame(['three', 'two', 'one'], array_keys($menu->getChildren()));
+        $this->assertSame(['two.two', 'two.one'], array_keys($menu->getChild('two')->getChildren()));
+    }
+}
