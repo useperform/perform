9e7ca5ec81c315884dc3ba4b5a014420c4b10125
Glynn Forrest <me@glynnforrest.com>
2018-06-08 10:17:52 +0000

    base: Replace settings link provider with event listener

---
+++ src/BaseBundle/EventListener/SettingsMenuListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Perform\BaseBundle\EventListener;
+
+use Perform\BaseBundle\Event\MenuEvent;
+use Perform\BaseBundle\Routing\RouteChecker;
+
+/**
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class SettingsMenuListener
+{
+    protected $routeChecker;
+
+    public function __construct(RouteChecker $routeChecker)
+    {
+        $this->routeChecker = $routeChecker;
+    }
+
+    public function onMenuBuild(MenuEvent $event)
+    {
+        if ($event->getMenuName() !== 'perform_sidebar') {
+            return;
+        }
+
+        if (!$this->routeChecker->routeExists('perform_base_settings_settings')) {
+            return;
+        }
+
+        $menu = $event->getMenu();
+
+        $menu->addChild('settings', [
+            'route' => 'perform_base_settings_settings',
+        ])->setExtra('icon', 'cogs');
+    }
+}
