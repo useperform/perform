cbfc0a7c40e9533158b652db217a67b15dded9f3
Glynn Forrest <me@glynnforrest.com>
2018-06-16 21:37:27 +0100

    base: Add a data collector for showing settings reads and writes

---
+++ src/BaseBundle/DataCollector/SettingsDataCollector.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Perform\BaseBundle\DataCollector;
+
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Perform\BaseBundle\Settings\Manager\TraceableManager;
+
+/**
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class SettingsDataCollector extends DataCollector
+{
+    protected $settings;
+
+    public function __construct(TraceableManager $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+        $this->data['getCalls'] = $this->settings->getGetCalls();
+        $this->data['setCalls'] = $this->settings->getSetCalls();
+        $this->data['used'] = isset($this->data['getCalls'][0]) || isset($this->data['setCalls'][0]);
+    }
+
+    public function reset()
+    {
+        $this->data = [];
+    }
+
+    public function getName()
+    {
+        return 'perform_base.settings';
+    }
+
+    public function getGetCalls()
+    {
+        return array_map(function ($data) {
+            return [
+                $data[0],
+                $this->cloneVar($data[1]),
+                $data[2],
+            ];
+        }, $this->data['getCalls']);
+    }
+
+    public function getDefaultGetCalls()
+    {
+        return array_filter($this->getGetCalls(), function ($data) {
+            return $data[2] === false;
+        });
+    }
+
+    public function getSetCalls()
+    {
+        return array_map(function ($data) {
+            return [
+                $data[0],
+                $this->cloneVar($data[1]),
+            ];
+        }, $this->data['setCalls']);
+    }
+
+    public function getUsed()
+    {
+        return $this->data['used'];
+    }
+}
