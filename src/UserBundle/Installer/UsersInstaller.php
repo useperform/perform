d75010dd33f82080efc282b50547459b5fb421b8
Glynn Forrest <me@glynnforrest.com>
2018-07-04 21:42:35 +0000

    user: Rework UsersInstaller for new interface

--- src/UserBundle/Installer/UsersInstaller.php
+++ src/UserBundle/Installer/UsersInstaller.php
@@ -5,22 +5,31 @@ namespace Perform\UserBundle\Installer;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Perform\BaseBundle\Installer\InstallerInterface;
+use Perform\UserBundle\Importer\UserImporter;
 
 /**
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
 class UsersInstaller implements InstallerInterface
 {
-    public function install(ContainerInterface $container, LoggerInterface $logger)
+    protected $importer;
+    protected $usersFile;
+
+    public function __construct(UserImporter $importer, $usersFile)
     {
-        $file = $container->getParameter('kernel.root_dir').'/config/perform_users.yml';
-        if (!file_exists($file)) {
+        $this->importer = $importer;
+        $this->usersFile = $usersFile;
+    }
+
+    public function install(LoggerInterface $logger)
+    {
+        if (!file_exists($this->usersFile)) {
+            $logger->debug(sprintf('Not importing users from <info>%s</info>, file not found', $this->usersFile));
             return;
         }
 
-        $logger->info(sprintf('Importing users from <info>%s</info>', $file));
-        $importer = $container->get('perform_user.importer.user');
-        $importer->importYamlFile($file);
+        $logger->info(sprintf('Importing users from <info>%s</info>', $this->usersFile));
+        $this->importer->importYamlFile($this->usersFile);
     }
 
     public function requiresConfiguration()
