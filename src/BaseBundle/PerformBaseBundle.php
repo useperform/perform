f15abbf0dd4febf54c0929be4d42345b2148e9d5
Glynn Forrest <me@glynnforrest.com>
2018-07-28 15:42:52 +0000

    base: Add pass to remove services if required services are missing

--- src/BaseBundle/PerformBaseBundle.php
+++ src/BaseBundle/PerformBaseBundle.php
@@ -12,15 +12,22 @@ use Perform\BaseBundle\DependencyInjection\Compiler\InstallersPass;
 use Perform\BaseBundle\DependencyInjection\Compiler\FieldTypesPass;
 use Perform\BaseBundle\DependencyInjection\Compiler\DoctrinePass;
 use Perform\BaseBundle\DependencyInjection\Compiler\FormTemplatesPass;
+use Perform\BaseBundle\DependencyInjection\Compiler\RequiredServicesPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 
 /**
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
 class PerformBaseBundle extends Bundle
 {
+    private static $serviceRequirements = [
+        'perform_base.twig.serializer' => ['serializer'],
+    ];
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
+        // high priority, run before any passes working with tags
+        $container->addCompilerPass(new RequiredServicesPass(static::$serviceRequirements), PassConfig::TYPE_BEFORE_OPTIMIZATION, 100);
         $container->addCompilerPass(new CrudPass());
         $container->addCompilerPass(new CrudVoterPass());
         $container->addCompilerPass(new SettingsPass());
