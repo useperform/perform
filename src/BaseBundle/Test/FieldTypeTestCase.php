c4d70bfd5241992de8c3f849864cf8f89e86df43
Glynn Forrest <me@glynnforrest.com>
2018-08-25 11:04:48 +0100

    base: Add helper methods to FieldTypeTestCase
    This makes it easy to add() a field and test the HTML it would show on render.

--- src/BaseBundle/Test/FieldTypeTestCase.php
+++ src/BaseBundle/Test/FieldTypeTestCase.php
@@ -6,6 +6,8 @@ use Perform\BaseBundle\FieldType\FieldTypeRegistry;
 use Perform\BaseBundle\Crud\ContextRenderer;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Twig\Environment;
+use Perform\BaseBundle\Config\FieldConfig;
+use Perform\BaseBundle\Crud\CrudRequest;
 
 /**
  * @author Glynn Forrest <me@glynnforrest.com>
@@ -14,6 +16,7 @@ abstract class FieldTypeTestCase extends \PHPUnit_Framework_TestCase
 {
     protected $kernel;
     protected $typeRegistry;
+    protected $config;
     protected $renderer;
 
     public function setUp()
@@ -23,8 +26,9 @@ abstract class FieldTypeTestCase extends \PHPUnit_Framework_TestCase
         $this->kernel->boot();
         $twig = $this->kernel->getContainer()->get('twig');
 
-        // but create a fresh type registry and container for testing
+        // but create a fresh type registry, config, and renderer for testing
         $this->typeRegistry = Services::fieldTypeRegistry($this->registerTypes());
+        $this->config = new FieldConfig($this->typeRegistry);
         $this->renderer = new ContextRenderer($this->typeRegistry, $twig);
     }
 
@@ -37,4 +41,32 @@ abstract class FieldTypeTestCase extends \PHPUnit_Framework_TestCase
      * @return TypeInterface[]
      */
     abstract protected function registerTypes();
+
+    /**
+     * Get a named type from the test FieldTypeRegistry.
+     */
+    protected function getType($name)
+    {
+        return $this->typeRegistry->getType($name);
+    }
+
+    /**
+     * Return the result of rendering a field in the list context.
+     *
+     * You should have added the field with $this->config->add().
+     */
+    protected function listContext($entity, $field)
+    {
+        return $this->renderer->listContext($entity, $field, $this->config->getTypes(CrudRequest::CONTEXT_LIST)[$field]);
+    }
+
+    /**
+     * Return the result of rendering a field in the view context.
+     *
+     * You should have added the field with $this->config->add().
+     */
+    protected function viewContext($entity, $field)
+    {
+        return $this->renderer->viewContext($entity, $field, $this->config->getTypes(CrudRequest::CONTEXT_VIEW)[$field]);
+    }
 }
