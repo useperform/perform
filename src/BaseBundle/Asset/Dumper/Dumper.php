1feae95295ba1ec52e8bd84da3a1ffe356f51ebb
Glynn Forrest <glynn@backbeat.tech>
2018-12-11 22:38:58 +0000

    base: Only dump asset files if their contents have changed
    This stops processes that watch them performing work for reason,
    e.g. webpack rebuilding assets on a file change.

--- src/BaseBundle/Asset/Dumper/Dumper.php
+++ src/BaseBundle/Asset/Dumper/Dumper.php
@@ -21,6 +21,13 @@ class Dumper
      */
     public function dump(TargetInterface $target)
     {
-        $this->fs->dumpFile($target->getFilename(), $target->getContents());
+        // do a hash check first, only dump if the contents have changed
+        // this stops processes watching the file performing work for no reason
+        // (e.g. webpack rebuilding assets on change)
+        $newHash = md5($target->getContents());
+        $existingHash = file_exists($target->getFilename()) ? md5(file_get_contents($target->getFilename())) : '';
+        if ($existingHash !== $newHash) {
+            $this->fs->dumpFile($target->getFilename(), $target->getContents());
+        }
     }
 }
