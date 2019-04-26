f1b25fdcc5ade46062463620f4165e2b40c6620a
Glynn Forrest <glynn@backbeat.tech>
2019-04-26 19:22:28 +0100

    Simplify composer config tests

--- src/BaseBundle/Test/ComposerConfigTestCase.php
+++ src/BaseBundle/Test/ComposerConfigTestCase.php
@@ -4,11 +4,6 @@ namespace Perform\BaseBundle\Test;
 
 use PHPUnit\Framework\TestCase;
 
-/**
- * ComposerConfigTestCase.
- *
- * @author Glynn Forrest <me@glynnforrest.com>
- **/
 abstract class ComposerConfigTestCase extends TestCase
 {
     abstract protected function getPackageName();
@@ -27,14 +22,10 @@ abstract class ComposerConfigTestCase extends TestCase
         return $config;
     }
 
-    protected function getParentConfig()
-    {
-        return json_decode(file_get_contents(__DIR__.'/../../../composer.json'), true);
-    }
-
     public function testName()
     {
         $this->assertSame($this->getPackageName(), $this->getBundleConfig()['name']);
+        $this->assertSame('perform/', substr($this->getPackageName(), 0, 8));
     }
 
     public function testLicense()
@@ -44,63 +35,8 @@ abstract class ComposerConfigTestCase extends TestCase
 
     public function testAutoload()
     {
-        //namespaces must ends with a backslash to be psr4 compatible
+        //namespaces must end with a backslash to be psr4 compatible
         $namespace = trim($this->getNamespace(), '\\').'\\';
         $this->assertSame([$namespace => ''], $this->getBundleConfig()['autoload']['psr-4']);
-    }
-
-    public function testReplaceIsDefinedInParentConfig()
-    {
-        $parentConfig = $this->getParentConfig();
-        $name = $this->getPackageName();
-        $this->assertArrayHasKey($name, $parentConfig['replace'], sprintf('Parent composer.json does not replace bundle "%s"', $name));
-        $this->assertSame('self.version', $parentConfig['replace'][$name], 'Parent composer.json must use self.version when replacing bundles');
-    }
-
-    public function testRequireVersionsMatch()
-    {
-        $bundleConfig = $this->getBundleConfig();
-        $parentConfig = $this->getParentConfig();
-
-        $bundleDeps = isset($bundleConfig['require']) ? $bundleConfig['require'] : [];
-        $parentDeps = $parentConfig['require'];
-
-        //check that every dependency is in the parent config, except for other perform bundles
-        foreach ($bundleDeps as $dep => $version) {
-            if (substr($dep, 0, 8) === 'perform/') {
-                $this->assertSame('self.version', $version, 'Version of another perform bundle dependency must be "self.version"');
-                continue;
-            }
-
-            $this->assertArrayHasKey($dep, $parentDeps, sprintf('Parent composer.json does not contain dependency "%s"', $dep));
-            $this->assertSame($parentDeps[$dep], $version, sprintf('Required version of "%s" does not match parent composer.json', $dep));
-        }
-    }
-
-    public function testRequireDevVersionsMatch()
-    {
-        $bundleConfig = $this->getBundleConfig();
-        $parentConfig = $this->getParentConfig();
-
-        $bundleDeps = isset($bundleConfig['require-dev']) ? $bundleConfig['require-dev'] : [];
-        $parentDeps = $parentConfig['require-dev'];
-
-        foreach ($bundleDeps as $dep => $version) {
-            $this->assertArrayHasKey($dep, $parentDeps, sprintf('Parent composer.json does not contain dev dependency "%s"', $dep));
-            $this->assertSame($parentDeps[$dep], $version, sprintf('Required dev version of "%s" does not match parent composer.json', $dep));
-        }
-    }
-
-    public function testSuggestsAreIncludedInParent()
-    {
-        $bundleConfig = $this->getBundleConfig();
-        $parentConfig = $this->getParentConfig();
-
-        $bundleSuggestions = isset($bundleConfig['suggest']) ? $bundleConfig['suggest'] : [];
-        $parentPackages = array_keys(array_merge($parentConfig['suggest'], $parentConfig['require']));
-
-        foreach (array_keys($bundleSuggestions) as $suggestion) {
-            $this->assertContains($suggestion, $parentPackages, sprintf('Parent composer.json does not suggest or require suggested dependency "%s"', $suggestion));
-        }
     }
 }
