e680fe71f6750761b7e0bf801d18aff6bb1db260
Glynn Forrest <glynn@backbeat.tech>
2019-06-15 16:25:53 +0100

    dev: Use npm dependency services for merging package.json

---
+++ src/DevBundle/DependencyInjection/Compiler/NpmDependenciesPass.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Perform\DevBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+use Perform\DevBundle\Command\MergeNpmPackagesCommand;
+
+class NpmDependenciesPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $deps = [];
+        foreach (array_keys($container->findTaggedServiceIds('perform_dev.npm_dependencies')) as $service) {
+            $deps[] = new Reference($service);
+        }
+        $container->getDefinition(MergeNpmPackagesCommand::class)
+            ->setArgument(1, $deps);
+    }
+}
