02d5af869eaf5ee59e7550f0cfb8ba50943f5593
Glynn Forrest <glynn@backbeat.tech>
2019-06-21 13:42:51 +0100

    rich-content: Replace package.json with a dependencies class

--- src/RichContentBundle/DependencyInjection/PerformRichContentExtension.php
+++ src/RichContentBundle/DependencyInjection/PerformRichContentExtension.php
@@ -9,6 +9,7 @@ use Symfony\Component\DependencyInjection\Loader;
 use Perform\RichContentBundle\DataFixtures\Profile\ProfileInterface;
 use Perform\BaseBundle\DependencyInjection\Assets;
 use Perform\BaseBundle\DependencyInjection\Compiler\FormTemplatesPass;
+use Perform\DevBundle\PerformDevBundle;
 
 /**
  * @author Glynn Forrest <me@glynnforrest.com>
@@ -27,9 +28,12 @@ class PerformRichContentExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        if (\class_exists(PerformDevBundle::class)) {
+            $loader->load('services/dev.yml');
+        }
+
         $container->registerForAutoconfiguration(ProfileInterface::class)->addTag('perform_rich_content.fixture_profile');
         Assets::addNamespace($container, 'perform-rich-content', __DIR__.'/../Resources');
-        Assets::addNpmConfig($container, __DIR__.'/../package.json');
         Assets::addExtraSass($container, '~perform-rich-content/scss/components.scss');
         Assets::addExtraJavascript($container, 'richContent', __DIR__.'/../Resources/js/module.js');
         FormTemplatesPass::addTemplate($container, '@PerformRichContent/form_types.html.twig');
