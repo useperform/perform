55d80ccd7e2f46885c70d4ac10f58f1fad9f2cb8
Glynn Forrest <glynn@backbeat.tech>
2019-06-21 14:09:31 +0100

    dev: Give command a more appropriate name

--- src/DevBundle/DependencyInjection/Compiler/NpmDependenciesPass.php
+++ src/DevBundle/DependencyInjection/Compiler/NpmDependenciesPass.php
@@ -5,7 +5,7 @@ namespace Perform\DevBundle\DependencyInjection\Compiler;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
-use Perform\DevBundle\Command\MergeNpmPackagesCommand;
+use Perform\DevBundle\Command\UpdateNpmDependenciesCommand;
 
 class NpmDependenciesPass implements CompilerPassInterface
 {
@@ -15,7 +15,7 @@ class NpmDependenciesPass implements CompilerPassInterface
         foreach (array_keys($container->findTaggedServiceIds('perform_dev.npm_dependencies')) as $service) {
             $deps[] = new Reference($service);
         }
-        $container->getDefinition(MergeNpmPackagesCommand::class)
+        $container->getDefinition(UpdateNpmDependenciesCommand::class)
             ->setArgument(1, $deps);
     }
 }
