6bc60abb44191a24c523e534b33668c4761b5e41
Glynn Forrest <glynn@backbeat.tech>
2019-06-23 16:10:36 +0100

    Move gulp and npm config to root of docs directory

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -8,30 +8,31 @@ function handleError (err) {
   this.emit('end');
 }
 
+var themeDir = '_themes/perform';
+
 gulp.task('js', function () {
   return gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
-    .pipe(gulp.dest('static/'));
+    .pipe(gulp.dest(themeDir+'/static/'));
 });
 
 gulp.task('sass', function () {
   return gulp.src('scss/theme.scss')
     .pipe(sass())
     .on('error', handleError)
-    .pipe(gulp.dest('static/'))
+    .pipe(gulp.dest(themeDir+'/static/'))
   // need to pipe to the build output directory so browsersync works
-    .pipe(gulp.dest('../../_build/html/_static/'))
+    .pipe(gulp.dest('_build/html/_static/'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
   gulp.watch('scss/*.scss', ['sass']);
-  gulp.watch('../../**/*.rst', ['html']);
-  gulp.watch('*.html', ['html']);
+  gulp.watch('**/*.rst', ['html']);
+  gulp.watch(themeDir+'/*.html', ['html']);
 });
 
 gulp.task('html', function () {
   run('make html', {
-    cwd: '../../'
   })
     .exec()
     .on('error', handleError);
@@ -41,7 +42,7 @@ gulp.task('browser-sync', function () {
   browserSync.init({
     open: false,
     server: {
-      baseDir: "../../_build/html"
+      baseDir: "_build/html"
     }
   });
 });
