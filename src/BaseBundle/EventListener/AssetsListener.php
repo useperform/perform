0776d95eeaf0370d28eb28000e5b97eccec3afba
Glynn Forrest <glynn@backbeat.tech>
2019-07-26 13:10:19 +0100

    base: Don't generate any scss files

--- src/BaseBundle/EventListener/AssetsListener.php
+++ src/BaseBundle/EventListener/AssetsListener.php
@@ -3,7 +3,6 @@
 namespace Perform\BaseBundle\EventListener;
 
 use Perform\BaseBundle\Asset\Dumper\JavascriptTarget;
-use Perform\BaseBundle\Asset\Dumper\SassTarget;
 use Perform\BaseBundle\Event\AssetDumpEvent;
 
 /**
@@ -32,9 +31,6 @@ class AssetsListener
 
     public function onAddAssets(AssetDumpEvent $event)
     {
-        $event->addTarget(new SassTarget(self::BUNDLE_DIR.'/Resources/scss/_extras.scss', $this->extraSass));
-        $event->addTarget(new SassTarget(self::BUNDLE_DIR.'/Resources/scss/_theme.scss', [$this->theme.'/theme.scss']));
-        $event->addTarget(new SassTarget(self::BUNDLE_DIR.'/Resources/scss/_theme_variables.scss', [$this->theme.'/variables.scss']));
         $event->addTarget(new JavascriptTarget(self::BUNDLE_DIR.'/Resources/src/modules.js', $this->jsModules));
     }
 }
